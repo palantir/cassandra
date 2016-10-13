72d6f263d56b4a1f7d93bf44887f3306ea9ef6ca
Dikang Gu <dikang85@gmail.com>
2016-10-13 12:31:39 -0700

    Implement the NoReplicationTokenAllocator
    Patch by Dikang Gu; reviewed by Branimir Lambov for CASSANDRA-12777

--- src/java/org/apache/cassandra/dht/tokenallocator/TokenAllocation.java
+++ src/java/org/apache/cassandra/dht/tokenallocator/TokenAllocation.java
@@ -64,8 +64,8 @@ public class TokenAllocation
             SummaryStatistics os = replicatedOwnershipStats(tokenMetadataCopy, rs, endpoint);
             tokenMetadataCopy.updateNormalTokens(tokens, endpoint);
             SummaryStatistics ns = replicatedOwnershipStats(tokenMetadataCopy, rs, endpoint);
-            logger.warn("Replicated node load in datacentre before allocation " + statToString(os));
-            logger.warn("Replicated node load in datacentre after allocation " + statToString(ns));
+            logger.warn("Replicated node load in datacentre before allocation {}", statToString(os));
+            logger.warn("Replicated node load in datacentre after allocation {}", statToString(ns));
 
             // TODO: Is it worth doing the replicated ownership calculation always to be able to raise this alarm?
             if (ns.getStandardDeviation() > os.getStandardDeviation())
@@ -149,7 +149,7 @@ public class TokenAllocation
             if (strategy.inAllocationRing(en.getValue()))
                 sortedTokens.put(en.getKey(), en.getValue());
         }
-        return new ReplicationAwareTokenAllocator<>(sortedTokens, strategy, partitioner);
+        return TokenAllocatorFactory.createTokenAllocator(sortedTokens, strategy, partitioner);
     }
 
     interface StrategyAdapter extends ReplicationStrategy<InetAddress>
