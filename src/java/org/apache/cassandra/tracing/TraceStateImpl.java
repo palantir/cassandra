29505d401c8d7c51f57cda7aa0d58aafad7e4e36
Stefania Alborghetti <stefania.alborghetti@datastax.com>
2016-10-25 09:43:17 +0800

    Change cassandra.wait_for_tracing_events_timeout_secs default to 0
    Was originally the merge commit: Merge branch 'cassandra-3.0' into cassandra-3.X
    Ignored changes to CHANGES.txt
    Resolved conflicts in backport caused by CASSANDRA-11425 (which isn't on 2.2.18)

--- src/java/org/apache/cassandra/tracing/TraceStateImpl.java
+++ src/java/org/apache/cassandra/tracing/TraceStateImpl.java
@@ -23,6 +23,7 @@ import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -42,8 +43,9 @@ import org.apache.cassandra.utils.WrappedRunnable;
 public class TraceStateImpl extends TraceState
 {
     private static final Logger logger = LoggerFactory.getLogger(TraceStateImpl.class);
-    private static final int WAIT_FOR_PENDING_EVENTS_TIMEOUT_SECS =
-      Integer.valueOf(System.getProperty("cassandra.wait_for_tracing_events_timeout_secs", "1"));
+
+    private static int WAIT_FOR_PENDING_EVENTS_TIMEOUT_SECS =
+      Integer.parseInt(System.getProperty("cassandra.wait_for_tracing_events_timeout_secs", "0"));
 
     public TraceStateImpl(InetAddress coordinator, UUID sessionId, Tracing.TraceType traceType)
     {
