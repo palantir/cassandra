ShoOgino/cassandraFile

Upgrade Guava to 23.3 and Airline to 0.8
Patch by marcuse; reviewed by Stefan Podkowinski for CASSANDRA-13997

src/java/org/apache/cassandra/repair/RepairRunnable.java
@@ -330,7 +330,7 @@ public class RepairRunnable extends WrappedRunnable implements ProgressEventNoti
         // run anticompaction if necessary and send finish notice back to client
         final Collection<Range<Token>> successfulRanges = new ArrayList<>();
         final AtomicBoolean hasFailure = new AtomicBoolean();
-        ListenableFuture repairResult = Futures.transform(allSessions, new AsyncFunction<List<RepairSessionResult>, Object>()
+        ListenableFuture repairResult = Futures.transformAsync(allSessions, new AsyncFunction<List<RepairSessionResult>, Object>()
         {
             @SuppressWarnings("unchecked")
             public ListenableFuture apply(List<RepairSessionResult> results)
@@ -354,8 +354,8 @@ public class RepairRunnable extends WrappedRunnable implements ProgressEventNoti
                 }
                 return Futures.immediateFuture(null);
             }
-        });
-        Futures.addCallback(repairResult, new RepairCompleteCallback(parentSession, successfulRanges, startTime, traceState, hasFailure, executor));
+        }, MoreExecutors.directExecutor());
+        Futures.addCallback(repairResult, new RepairCompleteCallback(parentSession, successfulRanges, startTime, traceState, hasFailure, executor), MoreExecutors.directExecutor());
     }
 
     /**

palantir/cassandra

src/java/org/apache/cassandra/repair/RepairRunnable.java
@@ -271,7 +271,7 @@ public class RepairRunnable extends WrappedRunnable implements ProgressEventNoti
                                                              totalProgress,
                                                              message));
                 }
-            });
+            }, MoreExecutors.directExecutor());
             futures.add(session);
         }
 
@@ -280,7 +280,7 @@ public class RepairRunnable extends WrappedRunnable implements ProgressEventNoti
         final Collection<Range<Token>> successfulRanges = new ArrayList<>();
         final AtomicBoolean hasFailure = new AtomicBoolean();
         final ListenableFuture<List<RepairSessionResult>> allSessions = Futures.successfulAsList(futures);
-        ListenableFuture anticompactionResult = Futures.transform(allSessions, new AsyncFunction<List<RepairSessionResult>, Object>()
+        ListenableFuture anticompactionResult = Futures.transformAsync(allSessions, new AsyncFunction<List<RepairSessionResult>, Object>()
         {
             @SuppressWarnings("unchecked")
             public ListenableFuture apply(List<RepairSessionResult> results) throws Exception
@@ -299,7 +299,7 @@ public class RepairRunnable extends WrappedRunnable implements ProgressEventNoti
                 }
                 return ActiveRepairService.instance.finishParentSession(parentSession, allNeighbors, successfulRanges);
             }
-        });
+        }, MoreExecutors.directExecutor());
         Futures.addCallback(anticompactionResult, new FutureCallback<Object>()
         {
             public void onSuccess(Object result)
@@ -347,7 +347,7 @@ public class RepairRunnable extends WrappedRunnable implements ProgressEventNoti
                 }
                 executor.shutdownNow();
             }
-        });
+        }, MoreExecutors.directExecutor());
     }
 
     private Thread createQueryThread(final int cmd, final UUID sessionId)