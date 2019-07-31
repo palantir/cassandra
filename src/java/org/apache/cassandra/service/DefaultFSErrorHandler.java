c4d5b6c668202fbc2bdf218294b15e923bfeabde
Mike Moldawsky <mmoldawsky@palantir.com>
2019-07-31 17:11:29 +0300

    Revert: remove register of sstable corruption & fs error

--- src/java/org/apache/cassandra/service/DefaultFSErrorHandler.java
+++ src/java/org/apache/cassandra/service/DefaultFSErrorHandler.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.service;
 
 import java.io.File;
 
+import com.google.common.collect.ImmutableMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -45,6 +46,11 @@ public class DefaultFSErrorHandler implements FSErrorHandler
         JVMStabilityInspector.inspectThrowable(e);
         switch (DatabaseDescriptor.getDiskFailurePolicy())
         {
+            case stop:
+                StorageService.instance.recordNonTransientError(
+                        StorageService.NonTransientError.SSTABLE_CORRUPTION,
+                        ImmutableMap.of("path", e.path.toString()));
+                break;
             case stop_paranoid:
                 StorageService.instance.stopTransports();
                 break;
@@ -63,6 +69,9 @@ public class DefaultFSErrorHandler implements FSErrorHandler
             case stop_paranoid:
             case stop:
                 StorageService.instance.stopTransports();
+                StorageService.instance.recordNonTransientError(
+                        StorageService.NonTransientError.FS_ERROR,
+                        ImmutableMap.of("path", e.path.toString()));
                 break;
             case best_effort:
                 // for both read and write errors mark the path as unwritable.
