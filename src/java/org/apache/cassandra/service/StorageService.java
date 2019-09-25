1dbbf86e9ce52e710cb7709adffaf16ae9e90525
Leon Zaruvinsky <lzaruvinsky@palantir.com>
2019-09-25 14:36:14 -0400

    Expose ephemeral snapshots (#77)
    * Expose ephemeral snapshots
    
    * Remove unnecessary function

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -18,7 +18,6 @@
 package org.apache.cassandra.service;
 
 import java.io.*;
-import java.lang.management.ManagementFactory;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
@@ -3120,7 +3119,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
      * @param tag
      *            the tag given to the snapshot; may not be null or empty
      * @param options
-     *            Map of options (skipFlush is the only supported option for now)
+     *            Map of options (skipFlush and ephemeral are supported)
      * @param entities
      *            list of keyspaces / tables in the form of empty | ks1 ks2 ... | ks1.cf1,ks2.cf2,...
      */
@@ -3128,6 +3127,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     public void takeSnapshot(String tag, Map<String, String> options, String... entities) throws IOException
     {
         boolean skipFlush = Boolean.parseBoolean(options.getOrDefault("skipFlush", "false"));
+        boolean ephemeral = Boolean.parseBoolean(options.getOrDefault("ephemeral", "false"));
 
         if (entities != null && entities.length > 0 && entities[0].contains("."))
         {
@@ -3135,7 +3135,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         }
         else
         {
-            takeSnapshot(tag, skipFlush, entities);
+            takeSnapshot(tag, skipFlush, ephemeral, entities);
         }
     }
 
@@ -3174,7 +3174,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
      */
     public void takeSnapshot(String tag, String... keyspaceNames) throws IOException
     {
-        takeSnapshot(tag, false, keyspaceNames);
+        takeSnapshot(tag, false, false, keyspaceNames);
     }
 
     /**
@@ -3196,9 +3196,10 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
      *
      * @param tag the tag given to the snapshot; may not be null or empty
      * @param skipFlush Skip blocking flush of memtable
+     * @param ephemeral Clear the snapshot on next startup
      * @param keyspaceNames the names of the keyspaces to snapshot; empty means "all."
      */
-    private void takeSnapshot(String tag, boolean skipFlush, String... keyspaceNames) throws IOException
+    private void takeSnapshot(String tag, boolean skipFlush, boolean ephemeral, String... keyspaceNames) throws IOException
     {
         if (operationMode == Mode.JOINING)
             throw new IOException("Cannot snapshot until bootstrap completes");
@@ -3225,7 +3226,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
 
         for (Keyspace keyspace : keyspaces)
-            keyspace.snapshot(tag, null, skipFlush);
+            keyspace.snapshot(tag, null, skipFlush, ephemeral);
     }
 
     /**
@@ -3288,7 +3289,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         for (Entry<Keyspace, List<String>> entry : keyspaceColumnfamily.entrySet())
         {
             for (String table : entry.getValue())
-                entry.getKey().snapshot(tag, table, skipFlush);
+                entry.getKey().snapshot(tag, table, skipFlush, false);
         }
 
     }
