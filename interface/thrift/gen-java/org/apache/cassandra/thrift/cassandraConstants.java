c5a8cea0ea05462b012e1300d22f8fe42a3383cf
Leon Zaruvinsky <lzaruvinsky@palantir.com>
2019-10-15 17:23:39 -0400

    Add put unless exists method to thrift interface (#71)
    * Add put unless exists method to thrift interface
    
    * Generated thrift code
    
    * add back license
    
    * fix throw on mv
    
    * rename expected to legacyUpdates; no opinion on empty updates
    
    * tests for cas and pue empty update noop semantics

--- interface/thrift/gen-java/org/apache/cassandra/thrift/cassandraConstants.java
+++ interface/thrift/gen-java/org/apache/cassandra/thrift/cassandraConstants.java
@@ -6,7 +6,7 @@
  */
 package org.apache.cassandra.thrift;
 /*
- * 
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -14,18 +14,17 @@ package org.apache.cassandra.thrift;
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
  * under the License.
- * 
+ *
  */
-
 
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.thrift.scheme.IScheme;
@@ -56,6 +55,6 @@ import org.slf4j.LoggerFactory;
 
 public class cassandraConstants {
 
-  public static final String VERSION = "20.1.0";
+  public static final String VERSION = "20.1.0-pt0";
 
 }
