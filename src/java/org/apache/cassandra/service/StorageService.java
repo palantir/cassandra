b7760b582585f7b256f29deed1f7994caab8ce79
Mattheo Ioannou <mattheoioannou@gmail.com>
2019-10-17 13:03:02 -0400

    Add created timestamp in millis to snapshot details (#99)
    * Add created timestamp in millis to snapshot details
    
    * Rename tabular data for more readability

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2973,7 +2973,13 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
                         snapshotMap.put(snapshotDetail.getKey(), data);
                     }
 
-                    SnapshotDetailsTabularData.from(snapshotDetail.getKey(), keyspace.getName(), cfStore.getColumnFamilyName(), snapshotDetail, data);
+                    SnapshotDetailsTabularData.from(
+                    		snapshotDetail.getKey(),
+							keyspace.getName(),
+							cfStore.getColumnFamilyName(),
+							snapshotDetail,
+							cfStore.getSnapshotCreationTime(snapshotDetail.getKey()),
+							data);
                 }
             }
         }
