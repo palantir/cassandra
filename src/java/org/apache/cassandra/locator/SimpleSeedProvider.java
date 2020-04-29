18cf577876e5337d4bf04b07fde8f4e5b2e568a8
Leon Zaruvinsky <lzaruvinsky@palantir.com>
2020-04-28 23:22:51 -0400

    SimpleSeedProvider only returns self if cluster is new

--- src/java/org/apache/cassandra/locator/SimpleSeedProvider.java
+++ src/java/org/apache/cassandra/locator/SimpleSeedProvider.java
@@ -24,8 +24,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.annotations.VisibleForTesting;
+
 import org.apache.cassandra.config.Config;
 import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.utils.FBUtilities;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -33,7 +37,19 @@ public class SimpleSeedProvider implements SeedProvider
 {
     private static final Logger logger = LoggerFactory.getLogger(SimpleSeedProvider.class);
 
-    public SimpleSeedProvider(Map<String, String> args) {}
+    // If this flag is not set to true, then node is required to attempt a bootstrap
+    private final boolean isNewCluster;
+
+    public SimpleSeedProvider(Map<String, String> args)
+    {
+        isNewCluster = Boolean.getBoolean("palantir_cassandra.is_new_cluster");
+    }
+
+    @VisibleForTesting
+    public SimpleSeedProvider(boolean isNewCluster)
+    {
+        this.isNewCluster = isNewCluster;
+    }
 
     public List<InetAddress> getSeeds()
     {
@@ -47,12 +63,23 @@ public class SimpleSeedProvider implements SeedProvider
             throw new AssertionError(e);
         }
         String[] hosts = conf.seed_provider.parameters.get("seeds").split(",", -1);
+        InetAddress self = FBUtilities.getBroadcastAddress();
+        return getSeeds(hosts, self);
+    }
+
+    @VisibleForTesting
+    List<InetAddress> getSeeds(String[] hosts, InetAddress self) {
+        // Add all seeds except this node
         List<InetAddress> seeds = new ArrayList<InetAddress>(hosts.length);
         for (String host : hosts)
         {
             try
             {
-                seeds.add(InetAddress.getByName(host.trim()));
+                InetAddress seed = InetAddress.getByName(host.trim());
+                if (!self.equals(seed))
+                {
+                    seeds.add(seed);
+                }
             }
             catch (UnknownHostException ex)
             {
@@ -60,6 +87,12 @@ public class SimpleSeedProvider implements SeedProvider
                 logger.warn("Seed provider couldn't lookup host {}", host);
             }
         }
+
+        if (seeds.isEmpty() && isNewCluster)
+        {
+            seeds.add(self);
+        }
+
         return Collections.unmodifiableList(seeds);
     }
 }
