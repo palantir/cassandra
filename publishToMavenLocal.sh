baaf42ef1e9df2ec1c600c036ccfe50a0fa9f318
Sam Kramer <skramer@palantir.com>
2020-09-01 15:48:19 +0100

    publish script

---
+++ publishToMavenLocal.sh
@@ -0,0 +1,9 @@
+ant clean
+./gradlew clean
+ant
+ant generate-idea-files
+ant maven-ant-tasks-retrieve-build
+./gradlew --no-daemon
+ant build build-test
+./gradlew --no-daemon ant-artifacts
+./gradlew publishToMavenLocal
