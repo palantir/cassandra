5bc16a8b3f1c41cb84e2682ca763ac603b800b2d
Zachary Pear <zpear@palantir.com>
2021-09-03 17:38:15 -0400

    Add PPAM-Syn Verb Handler

--- src/java/com/palantir/cassandra/ppam/PrivatePublicAddressMappingSynVerbHandler.java
+++ src/java/com/palantir/cassandra/ppam/PrivatePublicAddressMappingSynVerbHandler.java
@@ -18,6 +18,57 @@
 
 package com.palantir.cassandra.ppam;
 
-public class PrivatePublicAddressMappingSynVerbHandler
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
+
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.Sets;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.net.IVerbHandler;
+import org.apache.cassandra.net.MessageIn;
+import org.apache.cassandra.net.MessageOut;
+import org.apache.cassandra.net.MessagingService;
+import org.apache.cassandra.utils.FBUtilities;
+
+public class PrivatePublicAddressMappingSynVerbHandler implements IVerbHandler<PrivatePublicAddressMappingSyn>
 {
+    private static final Logger logger = LoggerFactory.getLogger(PrivatePublicAddressMappingSynVerbHandler.class);
+    public void doVerb(MessageIn<PrivatePublicAddressMappingSyn> message, int id) throws UnknownHostException
+    {
+        PrivatePublicAddressMappingSyn synMessage = message.payload;
+        logger.trace("Handling new PPAM Syn message from {}/{}", message.from, synMessage.getSourceHostname());
+
+        InetAddressHostname sourceName = synMessage.getSourceHostname();
+        InetAddressIp sourceInternalIp = synMessage.getSourceInternalAddress();
+
+        InetAddressHostname targetName = synMessage.getTargetHostname();
+        InetAddressIp targetExternalIp = synMessage.getTargetExternalAddress();
+
+        // InetAddress.getByHostname performs a DNS lookup
+        InetAddressIp sourceExternalIp = new InetAddressIp(InetAddress.getByName(sourceName.toString()).getHostAddress());
+
+        InetAddressIp targetInternalIp = new InetAddressIp(FBUtilities.getBroadcastAddress().getHostAddress());
+
+        PrivatePublicAddressMappingCoordinator.instance.updatePrivatePublicAddressMapping(sourceName, sourceInternalIp, sourceExternalIp);
+
+        PrivatePublicAddressMappingAck ack = new PrivatePublicAddressMappingAck(targetName, targetInternalIp, targetExternalIp);
+        MessageOut<PrivatePublicAddressMappingAck> ackMessage = new MessageOut<>(
+                                                    MessagingService.Verb.PRIVATE_PUBLIC_ADDR_MAPPING_ACK,
+                                                    ack,
+                                                    PrivatePublicAddressMappingAck.serializer);
+        logger.trace("Sending PrivatePublicAddressMappingAck to {}", sourceExternalIp);
+        reply(ackMessage, sourceExternalIp);
+    }
+
+    @VisibleForTesting
+    void reply(MessageOut<PrivatePublicAddressMappingAck> ackMessage, InetAddressIp sourceExternal) throws UnknownHostException
+    {
+        MessagingService.instance().sendOneWay(ackMessage, InetAddress.getByName(sourceExternal.toString()));
+    }
 }
