ba8d5f4ac3277107b8eb7decf0f4365a81543c36
Zachary Pear <zpear@palantir.com>
2021-09-03 17:09:25 -0400

    Add PPAM-Ack

--- src/java/com/palantir/cassandra/ppam/PrivatePublicAddressMappingAck.java
+++ src/java/com/palantir/cassandra/ppam/PrivatePublicAddressMappingAck.java
@@ -18,6 +18,86 @@
 
 package com.palantir.cassandra.ppam;
 
+import java.io.DataInput;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Objects;
+
+import javax.annotation.Nonnull;
+
+import org.apache.cassandra.db.TypeSizes;
+import org.apache.cassandra.io.IVersionedSerializer;
+import org.apache.cassandra.io.util.DataOutputPlus;
+import org.apache.cassandra.net.CompactEndpointSerializationHelper;
+
 public class PrivatePublicAddressMappingAck
 {
+    // This name is pretty long :(
+    public static final IVersionedSerializer<PrivatePublicAddressMappingAck> serializer = new PrivatePublicAddressMappingAckSerializer();
+
+    // target node's hostname and VPC-internal IP / broadcast_address
+    private final InetAddressHostname targetHostname;
+    private final InetAddressIp targetInternalAddress;
+
+    // target node's resolved IP from source-node's DNS
+    private final InetAddressIp targetExternalAddress;
+
+    public PrivatePublicAddressMappingAck(@Nonnull InetAddressHostname targetHostname, @Nonnull InetAddressIp targetInternalAddress, @Nonnull InetAddressIp targetExternalAddress)
+    {
+        this.targetHostname = targetHostname;
+        this.targetExternalAddress = targetExternalAddress;
+        this.targetInternalAddress = targetInternalAddress;
+    }
+
+    public InetAddressHostname getTargetHostname()
+    {
+        return this.targetHostname;
+    }
+
+    public InetAddressIp getTargetExternalAddress()
+    {
+        return this.targetExternalAddress;
+    }
+
+    public InetAddressIp getTargetInternalAddress()
+    {
+        return this.targetInternalAddress;
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (!(o instanceof PrivatePublicAddressMappingAck)) return false;
+
+        PrivatePublicAddressMappingAck other = (PrivatePublicAddressMappingAck) o;
+        return Objects.equals(this.getTargetHostname(), other.getTargetHostname())
+                  && Objects.equals(this.getTargetExternalAddress(), other.getTargetExternalAddress())
+                  && Objects.equals(this.getTargetInternalAddress(), other.getTargetInternalAddress());
+    }
+}
+
+class PrivatePublicAddressMappingAckSerializer
+implements IVersionedSerializer<PrivatePublicAddressMappingAck>
+{
+    public void serialize(PrivatePublicAddressMappingAck ackMessage, DataOutputPlus out, int version) throws IOException
+    {
+        out.writeUTF(ackMessage.getTargetHostname().toString());
+        out.writeUTF(ackMessage.getTargetInternalAddress().toString());
+        out.writeUTF(ackMessage.getTargetExternalAddress().toString());
+    }
+
+    public PrivatePublicAddressMappingAck deserialize(DataInput in, int version) throws IOException
+    {
+        InetAddressHostname targetHostname = new InetAddressHostname(in.readUTF());
+        InetAddressIp targetInternalAddress = new InetAddressIp(in.readUTF());
+        InetAddressIp targetExternalAddress = new InetAddressIp(in.readUTF());
+        return new PrivatePublicAddressMappingAck(targetHostname, targetInternalAddress, targetExternalAddress);
+    }
+
+    public long serializedSize(PrivatePublicAddressMappingAck ack, int version)
+    {
+        return TypeSizes.NATIVE.sizeof(ack.getTargetHostname().toString())
+                    + TypeSizes.NATIVE.sizeof(ack.getTargetInternalAddress().toString())
+                    + TypeSizes.NATIVE.sizeof(ack.getTargetExternalAddress().toString());
+    }
 }
