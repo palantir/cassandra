4164f32c7155df7c658c96e3c04060c74aef8092
Sam Kramer <skramer@palantir.com>
2021-11-16 14:46:26 +0000

    add test

--- src/java/com/palantir/cassandra/utils/LockKeyspaceUtils.java
+++ src/java/com/palantir/cassandra/utils/LockKeyspaceUtils.java
@@ -25,7 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.config.DatabaseDescriptor;
-import org.apache.cassandra.db.SystemKeyspace;
 
 public class LockKeyspaceUtils
 {
