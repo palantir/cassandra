37d7aebf0270870cb2e211dce26be72ccb791535
Zachary Pear <zpear@palantir.com>
2022-03-09 12:34:18 -0500

    Add repair tracker unit tests

---
+++ test/unit/org/apache/cassandra/repair/RepairTrackerTest.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.repair;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import org.antlr.analysis.SemanticContext;
+import org.apache.cassandra.dht.Murmur3Partitioner;
+import org.apache.cassandra.repair.messages.RepairOption;
+import org.apache.cassandra.utils.progress.ProgressState;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class RepairTrackerTest
+{
+    private final RepairOption options = RepairOption.parse(new HashMap<>(), Murmur3Partitioner.instance);
+    private final RepairArguments args = new RepairArguments("test", options);
+    private RepairTracker tracker;
+
+    @Test
+    public void getRepairState_unknownIfCommandNotTracked()
+    {
+        tracker = new RepairTracker();
+        assertThat(tracker.getRepairState(1)).isEqualTo(ProgressState.UNKNOWN);
+    }
+
+    @Test
+    public void getRepairState_returnsExpected()
+    {
+        tracker = new RepairTracker();
+        RepairRunnable task = mock(RepairRunnable.class);
+        doReturn(ProgressState.FAILED).when(task).getCurrentState();
+        tracker.track(1, args, task);
+
+        RepairRunnable task2 = mock(RepairRunnable.class);
+        doReturn(ProgressState.SUCCEEDED).when(task2).getCurrentState();
+        tracker.track(2, args, task2);
+
+        assertThat(tracker.getRepairState(1)).isEqualTo(ProgressState.FAILED);
+        assertThat(tracker.getRepairState(2)).isEqualTo(ProgressState.SUCCEEDED);
+    }
+
+    @Test
+    public void getInProgressRepair_emptyWhenNotTracked()
+    {
+        tracker = new RepairTracker();
+        RepairArguments args = mock(RepairArguments.class);
+        assertThat(tracker.getInProgressRepair(args)).isEmpty();
+    }
+
+    @Test
+    public void getInProgressRepair_onlyReturnsInProgress()
+    {
+        tracker = new RepairTracker();
+        RepairRunnable task = mock(RepairRunnable.class);
+        doReturn(1).when(task).getCommand();
+        tracker.track(1, args, task);
+
+
+        Set<ProgressState> nonInProgress = Arrays.stream(ProgressState.values())
+                                                 .filter(state -> ProgressState.IN_PROGRESS != (state))
+                                                 .collect(Collectors.toSet());
+
+        for (ProgressState state : nonInProgress)
+        {
+            doReturn(state).when(task).getCurrentState();
+            assertThat(tracker.getInProgressRepair(args)).isEmpty();
+        }
+
+        doReturn(ProgressState.IN_PROGRESS).when(task).getCurrentState();
+        assertThat(tracker.getInProgressRepair(args)).contains(1);
+    }
+
+    @Test
+    public void getInProgressRepair_onlyReturnsMostRecentlyTracked()
+    {
+        tracker = new RepairTracker();
+        RepairRunnable task1 = mock(RepairRunnable.class);
+        doReturn(1).when(task1).getCommand();
+        doReturn(ProgressState.IN_PROGRESS).when(task1).getCurrentState();
+        tracker.track(1, args, task1);
+
+        RepairRunnable task2 = mock(RepairRunnable.class);
+        doReturn(ProgressState.IN_PROGRESS).when(task2).getCurrentState();
+        doReturn(2).when(task2).getCommand();
+        tracker.track(2, args, task2);
+
+        assertThat(tracker.getInProgressRepair(args)).contains(2);
+    }
+
+    @Test
+    public void getInProgressRepair_matchesSameArgs()
+    {
+        tracker = new RepairTracker();
+        RepairRunnable task1 = mock(RepairRunnable.class);
+        doReturn(1).when(task1).getCommand();
+        doReturn(ProgressState.IN_PROGRESS).when(task1).getCurrentState();
+        tracker.track(1, args, task1);
+
+        RepairArguments newReference = new RepairArguments("test",
+                                                           RepairOption.parse(new HashMap<>(),
+                                                                              Murmur3Partitioner.instance));
+
+        assertThat(tracker.getInProgressRepair(newReference)).contains(1);
+    }
+}
