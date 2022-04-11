06d28b37aabb04738da11d5a84130df6e81ef50a
Sam Kramer <skramer@palantir.com>
2022-04-11 16:48:44 +0100

    do not bootstrap if previous data is found for keyspace

--- src/java/com/palantir/cassandra/db/BootstrappingSafetyException.java
+++ src/java/com/palantir/cassandra/db/BootstrappingSafetyException.java
@@ -21,42 +21,13 @@ package com.palantir.cassandra.db;
 import org.apache.cassandra.db.SystemKeyspace;
 
 /**
- * There is a risk of a node crashing during the bootstrapping process. On the next start the node will detect
- * it has already started to bootstrap via {@link SystemKeyspace#bootstrapInProgress()}. Continuing to bootstrap
- * after a previous failure poses risk of filling up the node's storage device(s). To prevent this a system var can
- * be supplied in the form of <code>palantir_cassandra.bootstrap_disk_usage_threshold_percentage</code> that represents
- * a threshold of how filled data dirs can be, before bootstrapping is prevented if previous failures are detected.
- *
- * If current data dir utilization exceeds the safety threshold, the <code>BootstrappingSafetyException</code> is
- * thrown which case the server is disabled and remains in <code>Mode.NON_TRANSIENT_ERROR</code>.
- *
- * @author lyubent
+ * An explicit exception to be used when it is unsafe to perform a bootstrap.
  */
-public class BootstrappingSafetyException extends RuntimeException {
+public class BootstrappingSafetyException extends RuntimeException
+{
 
-    private String mode;
-    private String mostUtilizedDataDir;
-    private Double utilization;
-    private Double threshold;
-
-    public BootstrappingSafetyException(String mode, String mostUtilizedDataDir, Double utilization, Double threshold)
+    public BootstrappingSafetyException(String message)
     {
-        this.mode = mode;
-        this.mostUtilizedDataDir = mostUtilizedDataDir;
-        this.utilization = utilization;
-        this.threshold = threshold;
-    }
-
-    public String getLocalizedMessage()
-    {
-        return getMessage();
-    }
-
-    public String getMessage()
-    {
-        String msg = "Preventing node from continuing after failed bootstrap as most utilized data_file_dir (%s) " +
-                     "is too full (%f) and exceeds palantir_cassandra.bootstrap_disk_usage_threshold_percentage (%f). " +
-                     "Node will stay in %s mode.";
-        return String.format(msg, mostUtilizedDataDir, utilization, threshold, mode);
+        super(message);
     }
 }
