609b20787bb0ce4eec96940600f1035e577a9624
Sam-Kramer <skramer@palantir.com>
2022-08-18 16:24:24 +0100

    Add fsync metrics (#364)
    * Add fsync metrics
    
    * rename metric factory name

---
+++ src/java/com/palantir/cassandra/fsync/FsyncMetrics.java
@@ -0,0 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.cassandra.fsync;
+
+import com.codahale.metrics.Counter;
+import org.apache.cassandra.metrics.DefaultNameFactory;
+import org.apache.cassandra.metrics.MetricNameFactory;
+
+import static org.apache.cassandra.metrics.CassandraMetricsRegistry.Metrics;
+
+public class FsyncMetrics
+{
+    private static final MetricNameFactory factory = new DefaultNameFactory("Fsync");
+
+    public final Counter successfulSyncs = Metrics.counter(factory.createMetricName("SuccessfulSyncs"));
+    public final Counter failedSyncs = Metrics.counter(factory.createMetricName("FailedSyncs"));
+}
