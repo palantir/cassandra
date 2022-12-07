55955002a1a42a5c9c91831bb4f08f98f99bd946
Zac Pear <zpear@palantir.com>
2022-12-07 18:26:11 -0500

    Test

--- test/unit/org/apache/cassandra/service/StartupChecksTest.java
+++ test/unit/org/apache/cassandra/service/StartupChecksTest.java
@@ -92,6 +92,15 @@ public class StartupChecksTest
     }
 
     @Test
+    public void failStartupIfIpMatchesAvoidIp()
+    {
+        startupChecks = startupChecks.withTest(StartupChecks.checkIp);
+        System.setProperty("palantir_cassandra.avoid_ip", "127.0.0.1");
+
+        verifyFailure(startupChecks, "Cannot start as current IP");
+    }
+
+    @Test
     public void compatibilityCheckIgnoresNonDbFiles() throws Exception
     {
         startupChecks = startupChecks.withTest(StartupChecks.checkSSTablesFormat);
