7197ab03c5953bd84090ceb4c6a37aad76c91c62
Ines Potier <ipotier@palantir.com>
2023-03-08 11:36:47 -0500

    Set grace period back to 4 days

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -696,7 +696,7 @@ public class DatabaseDescriptor
 
             try {
                 Path systemDirectoryPath = Paths.get(conf.data_file_directories[0] + "/system");
-                Instant fourDaysAgo = Instant.now().minus(4, ChronoUnit.HOURS);
+                Instant fourDaysAgo = Instant.now().minus(4, ChronoUnit.DAYS);
 
                 if (Files.exists(systemDirectoryPath) && Files.getLastModifiedTime(systemDirectoryPath).toInstant().isBefore(fourDaysAgo))
                     throw new ConfigurationException("is_new_cluster flag is still set to true at least 4 days after cluster creation."
