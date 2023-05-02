3631b7d5d61cc579cb2db31a6704715e090d608d
Raymond Huffman <rhuffman@palantir.com>
2023-05-02 21:20:35 +0000

    clarify logs and config names

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -102,7 +102,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 {
     private static final Logger logger = LoggerFactory.getLogger(StorageService.class);
     private static final boolean DISABLE_WAIT_TO_BOOTSTRAP = Boolean.getBoolean("palantir_cassandra.disable_wait_to_bootstrap");
-    private static final boolean DISABLE_WAIT_TO_FINISH_BOOTSTRAP = Boolean.getBoolean("palantir_cassandra.disable_wait_to_serve_requests");
+    private static final boolean DISABLE_WAIT_TO_FINISH_BOOTSTRAP = Boolean.getBoolean("palantir_cassandra.disable_wait_to_finish_bootstrap");
     private static final Integer BOOTSTRAP_DISK_USAGE_THRESHOLD = Integer.getInteger("palantir_cassandra.bootstrap_disk_usage_threshold_percentage");
 
     public static final int RING_DELAY = getRingDelay(); // delay after which we assume ring has stablized
@@ -1045,9 +1045,9 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         {
             if (dataAvailable)
             {
-                logger.info("Bootstrapping complete. Not becoming an active ring member. Use JMX (StorageService->joinRing()) " +
-                        "to finalize ring joining. Set palantir_cassandra.disable_wait_to_serve_requests=true to bypass " +
-                        "this check and join the ring immediately after bootstrapping.");
+                logger.info("Bootstrap almost complete. Not becoming an active ring member. Use JMX (StorageService->finishBootstrap()) " +
+                        "to finalize ring joining. Set palantir_cassandra.disable_wait_to_finish_bootstrap=true to bypass " +
+                        "this step and join the ring immediately after bootstrapping.");
                 finishBootstrapGuard.await();
                 finishJoiningRing(bootstrapTokens);
 
