dccc8a9019ad83b7a76bafc242bda294e60249e1
Shamith Pasula <spasula@palantir.com>
2024-06-05 16:48:39 -0400

    undid whitespace changes

--- src/java/org/apache/cassandra/hadoop/cql3/CqlConfigHelper.java
+++ src/java/org/apache/cassandra/hadoop/cql3/CqlConfigHelper.java
@@ -1,24 +1,24 @@
 package org.apache.cassandra.hadoop.cql3;
 /*
-*
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*
-*/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.KeyManagementException;
@@ -34,7 +34,6 @@ import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 
-import com.codahale.metrics.jmx.JmxReporter;
 import com.google.common.base.Optional;
 import com.palantir.cassandra.objects.Dropwizard4Cluster;
 import org.apache.commons.lang3.StringUtils;
@@ -326,7 +325,6 @@ public class CqlConfigHelper
                 .withPort(port)
                 .withCompression(ProtocolOptions.Compression.NONE);
 
-
         if (authProvider.isPresent())
             builder.withAuthProvider(authProvider.get());
         if (sslOptions.isPresent())
@@ -529,7 +527,7 @@ public class CqlConfigHelper
                 context = getSSLContext(truststorePath, truststorePassword, keystorePath, keystorePassword);
             }
             catch (UnrecoverableKeyException | KeyManagementException |
-                    NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException e)
+                   NoSuchAlgorithmException | KeyStoreException | CertificateException | IOException e)
             {
                 throw new RuntimeException(e);
             }
@@ -592,12 +590,12 @@ public class CqlConfigHelper
                                             Optional<String> truststorePassword,
                                             Optional<String> keystorePath,
                                             Optional<String> keystorePassword)
-    throws NoSuchAlgorithmException,
-           KeyStoreException,
-           CertificateException,
-           IOException,
-           UnrecoverableKeyException,
-           KeyManagementException
+            throws NoSuchAlgorithmException,
+            KeyStoreException,
+            CertificateException,
+            IOException,
+            UnrecoverableKeyException,
+            KeyManagementException
     {
         SSLContext ctx = SSLContext.getInstance("SSL");
 
@@ -626,8 +624,8 @@ public class CqlConfigHelper
         }
 
         ctx.init(kmf != null ? kmf.getKeyManagers() : null,
-                 tmf != null ? tmf.getTrustManagers() : null,
-                 new SecureRandom());
+                tmf != null ? tmf.getTrustManagers() : null,
+                new SecureRandom());
         return ctx;
     }
 }
