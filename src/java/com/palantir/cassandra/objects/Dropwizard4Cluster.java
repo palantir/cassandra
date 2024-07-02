0a4a8b19d3de1182d758895d5d8afb6d051d8e81
Shamith Pasula <spasula@palantir.com>
2024-07-02 11:52:10 -0400

    optional JmxReporter

--- src/java/com/palantir/cassandra/objects/Dropwizard4Cluster.java
+++ src/java/com/palantir/cassandra/objects/Dropwizard4Cluster.java
@@ -3,25 +3,25 @@ package com.palantir.cassandra.objects;
 import com.codahale.metrics.jmx.JmxReporter;
 import com.datastax.driver.core.Cluster;
 
+import java.util.Optional;
+
 // Necessary for upgrading from Dropwizard 3.x to Dropwizard 4.x
 // See https://docs.datastax.com/en/developer/java-driver/3.6/manual/metrics/index.html#metrics-4-compatibility
 public class Dropwizard4Cluster extends Cluster {
-    private JmxReporter reporter;
+    private final Optional<JmxReporter> maybeReporter;
 
     private Dropwizard4Cluster(Cluster.Builder builder) {
         super(builder);
-        if (this.getMetrics() != null && this.getMetrics().getRegistry() != null) {
-            this.reporter = JmxReporter.forRegistry(this.getMetrics().getRegistry())
-                    .inDomain(this.getClusterName() + "-metrics")
-                    .build();
-        }
+        this.maybeReporter = Optional.ofNullable(this.getMetrics().getRegistry())
+                .map(registry -> JmxReporter.forRegistry(registry)
+                        .inDomain(this.getClusterName() + "-metrics")
+                        .build());
+        this.maybeReporter.ifPresent(JmxReporter::start);
     }
 
     @Override
     public void close() {
-        if (reporter != null) {
-            reporter.close();
-        }
+        maybeReporter.ifPresent(JmxReporter::close);
         super.close();
     }
 
