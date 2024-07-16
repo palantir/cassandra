ShoOgino/cassandraFile

Add optional startup delay to wait until peers are ready
patch by jasobrown; reviewed by Ariel Weisberg for CASSANDRA-13993

src/java/org/apache/cassandra/net/StartupClusterConnectivityChecker.java
@@ -1,0 +1,171 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.net;
+
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.locator.InetAddressAndPort;
+import org.apache.cassandra.net.async.OutboundConnectionIdentifier;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.net.MessagingService.Verb.PING;
+
+public class StartupClusterConnectivityChecker
+{
+    private static final Logger logger = LoggerFactory.getLogger(StartupClusterConnectivityChecker.class);
+
+    enum State { CONTINUE, FINISH_SUCCESS, FINISH_TIMEOUT }
+
+    private final int targetPercent;
+    private final int timeoutSecs;
+    private final Predicate<InetAddressAndPort> gossipStatus;
+
+    public StartupClusterConnectivityChecker(int targetPercent, int timeoutSecs, Predicate<InetAddressAndPort> gossipStatus)
+    {
+        if (targetPercent < 0)
+        {
+            targetPercent = 0;
+        }
+        else if (targetPercent > 100)
+        {
+            targetPercent = 100;
+        }
+        this.targetPercent = targetPercent;
+
+        if (timeoutSecs < 0)
+        {
+            timeoutSecs = 1;
+        }
+        else if (timeoutSecs > 100)
+        {
+            logger.warn("setting the block-for-peers timeout (in seconds) to {} might be a bit excessive, but using it nonetheless", timeoutSecs);
+        }
+        this.timeoutSecs = timeoutSecs;
+
+        this.gossipStatus = gossipStatus;
+    }
+
+    public void execute(Set<InetAddressAndPort> peers)
+    {
+        if (peers == null || targetPercent == 0)
+            return;
+
+        // remove current node from the set
+        peers = peers.stream()
+                     .filter(peer -> !peer.equals(FBUtilities.getBroadcastAddressAndPort()))
+                     .collect(Collectors.toSet());
+
+        // don't block if there's no other nodes in the cluster (or we don't know about them)
+        if (peers.size() <= 0)
+            return;
+
+        logger.info("choosing to block until {}% of peers are marked alive and connections are established; max time to wait = {} seconds",
+                    targetPercent, timeoutSecs);
+
+        // first, send out a ping message to open up the non-gossip connections
+        final AtomicInteger connectedCount = sendPingMessages(peers);
+
+        final long startNanos = System.nanoTime();
+        final long expirationNanos = startNanos + TimeUnit.SECONDS.toNanos(timeoutSecs);
+        int completedRounds = 0;
+        while (checkStatus(peers, connectedCount, startNanos, expirationNanos < System.nanoTime(), completedRounds) == State.CONTINUE)
+        {
+            completedRounds++;
+            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.MICROSECONDS);
+        }
+    }
+
+    State checkStatus(Set<InetAddressAndPort> peers, AtomicInteger connectedCount, final long startNanos, boolean beyondExpiration, final int completedRounds)
+    {
+        long currentAlive = peers.stream().filter(gossipStatus).count();
+        float currentAlivePercent = ((float) currentAlive / (float) peers.size()) * 100;
+
+        // assume two connections to remote host that we care to track here (small msg & large msg)
+        final int totalConnectionsSize = peers.size() * 2;
+        final int connectionsCount = connectedCount.get();
+        float currentConnectedPercent = ((float) connectionsCount / (float) totalConnectionsSize) * 100;
+
+        if (currentAlivePercent >= targetPercent && currentConnectedPercent >= targetPercent)
+        {
+            logger.info("after {} milliseconds, found {}% ({} / {}) of peers as marked alive, " +
+                        "and {}% ({} / {}) of peers as connected, " +
+                        "both of which are above the desired threshold of {}%",
+                        TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos),
+                        currentAlivePercent, currentAlive, peers.size(),
+                        currentConnectedPercent, connectionsCount, totalConnectionsSize,
+                        targetPercent);
+            return State.FINISH_SUCCESS;
+        }
+
+        // perform at least two rounds of checking, else this is kinda useless (and the operator set the aliveTimeoutSecs too low)
+        if (completedRounds >= 2 && beyondExpiration)
+        {
+            logger.info("after {} milliseconds, found {}% ({} / {}) of peers as marked alive, " +
+                        "and {}% ({} / {}) of peers as connected, " +
+                        "one or both of which is below the desired threshold of {}%",
+                        TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos),
+                        currentAlivePercent, currentAlive, peers.size(),
+                        currentConnectedPercent, connectionsCount, totalConnectionsSize,
+                        targetPercent);
+            return State.FINISH_TIMEOUT;
+        }
+        return State.CONTINUE;
+    }
+
+    /**
+     * Sends a "connection warmup" message to each peer in the collection, on every {@link OutboundConnectionIdentifier.ConnectionType}
+     * used for internode messaging.
+     */
+    private AtomicInteger sendPingMessages(Set<InetAddressAndPort> peers)
+    {
+        AtomicInteger connectedCount = new AtomicInteger(0);
+        IAsyncCallback responseHandler = new IAsyncCallback()
+        {
+            @Override
+            public boolean isLatencyForSnitch()
+            {
+                return false;
+            }
+
+            @Override
+            public void response(MessageIn msg)
+            {
+                connectedCount.incrementAndGet();
+            }
+        };
+
+        MessageOut<PingMessage> smallChannelMessageOut = new MessageOut<>(PING, PingMessage.smallChannelMessage, PingMessage.serializer);
+        MessageOut<PingMessage> largeChannelMessageOut = new MessageOut<>(PING, PingMessage.largeChannelMessage, PingMessage.serializer);
+        for (InetAddressAndPort peer : peers)
+        {
+            MessagingService.instance().sendRR(smallChannelMessageOut, peer, responseHandler);
+            MessagingService.instance().sendRR(largeChannelMessageOut, peer, responseHandler);
+        }
+
+        return connectedCount;
+    }
+}

test/unit/org/apache/cassandra/net/StartupClusterConnectivityCheckerTest.java
@@ -1,0 +1,129 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.net;
+
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+
+import com.google.common.net.InetAddresses;
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.apache.cassandra.locator.InetAddressAndPort;
+
+public class StartupClusterConnectivityCheckerTest
+{
+    @Test
+    public void testConnectivity_SimpleHappyPath() throws UnknownHostException
+    {
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(70, 10, addr -> true);
+        int count = 10;
+        Set<InetAddressAndPort> peers = createNodes(count);
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.FINISH_SUCCESS,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(count * 2), System.nanoTime(), false, 0));
+    }
+
+    @Test
+    public void testConnectivity_SimpleContinue() throws UnknownHostException
+    {
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(70, 10, addr -> true);
+        int count = 10;
+        Set<InetAddressAndPort> peers = createNodes(count);
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.CONTINUE,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(0), System.nanoTime(), false, 0));
+    }
+
+    @Test
+    public void testConnectivity_Timeout() throws UnknownHostException
+    {
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(70, 10, addr -> true);
+        int count = 10;
+        Set<InetAddressAndPort> peers = createNodes(count);
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.CONTINUE,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(0), System.nanoTime(), false, 4));
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.FINISH_TIMEOUT,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(0), System.nanoTime(), true, 5));
+    }
+
+    @Test
+    public void testConnectivity_SimpleUpdating() throws UnknownHostException
+    {
+        UpdatablePredicate predicate = new UpdatablePredicate();
+        final int count = 100;
+        final int thresholdPercentage = 70;
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(thresholdPercentage, 10, predicate);
+        Set<InetAddressAndPort> peers = createNodes(count);
+
+        AtomicInteger connectedCount = new AtomicInteger();
+
+        for (int i = 0; i < count; i++)
+        {
+            predicate.reset(i);
+            connectedCount.set(i * 2);
+            StartupClusterConnectivityChecker.State expectedState = i < thresholdPercentage ?
+                                                                    StartupClusterConnectivityChecker.State.CONTINUE :
+                                                                    StartupClusterConnectivityChecker.State.FINISH_SUCCESS;
+            Assert.assertEquals("failed on iteration " + i,
+                                expectedState, connectivityChecker.checkStatus(peers, connectedCount, System.nanoTime(), false, i));
+        }
+    }
+
+    /**
+     * returns true until index = threshold, then returns false.
+     */
+    private class UpdatablePredicate implements Predicate<InetAddressAndPort>
+    {
+        int index;
+        int threshold;
+
+        void reset(int threshold)
+        {
+            index = 0;
+            this.threshold = threshold;
+        }
+
+        @Override
+        public boolean test(InetAddressAndPort inetAddressAndPort)
+        {
+            index++;
+            return index <= threshold;
+        }
+    }
+
+    private static Set<InetAddressAndPort> createNodes(int count) throws UnknownHostException
+    {
+        Set<InetAddressAndPort> nodes = new HashSet<>();
+
+        if (count < 1)
+            Assert.fail("need at least *one* node in the set!");
+
+        InetAddressAndPort node = InetAddressAndPort.getByName("127.0.0.1");
+        nodes.add(node);
+        for (int i = 1; i < count; i++)
+        {
+            node = InetAddressAndPort.getByAddress(InetAddresses.increment(node.address));
+            nodes.add(node);
+        }
+        return nodes;
+    }
+
+}

palantir/cassandra

src/java/org/apache/cassandra/net/StartupClusterConnectivityChecker.java
@@ -1,0 +1,171 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.net;
+
+import java.net.InetAddress;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.google.common.util.concurrent.Uninterruptibles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.net.async.OutboundConnectionIdentifier;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.net.MessagingService.Verb.PING;
+
+public class StartupClusterConnectivityChecker
+{
+    private static final Logger logger = LoggerFactory.getLogger(StartupClusterConnectivityChecker.class);
+
+    enum State { CONTINUE, FINISH_SUCCESS, FINISH_TIMEOUT }
+
+    private final int targetPercent;
+    private final int timeoutSecs;
+    private final Predicate<InetAddress> gossipStatus;
+
+    public StartupClusterConnectivityChecker(int targetPercent, int timeoutSecs, Predicate<InetAddress> gossipStatus)
+    {
+        if (targetPercent < 0)
+        {
+            targetPercent = 0;
+        }
+        else if (targetPercent > 100)
+        {
+            targetPercent = 100;
+        }
+        this.targetPercent = targetPercent;
+
+        if (timeoutSecs < 0)
+        {
+            timeoutSecs = 1;
+        }
+        else if (timeoutSecs > 100)
+        {
+            logger.warn("setting the block-for-peers timeout (in seconds) to {} might be a bit excessive, but using it nonetheless", timeoutSecs);
+        }
+        this.timeoutSecs = timeoutSecs;
+
+        this.gossipStatus = gossipStatus;
+    }
+
+    public void execute(Set<InetAddress> peers)
+    {
+        if (peers == null || targetPercent == 0)
+            return;
+
+        // remove current node from the set
+        peers = peers.stream()
+                     .filter(peer -> !peer.equals(FBUtilities.getBroadcastAddress()))
+                     .collect(Collectors.toSet());
+
+        // don't block if there's no other nodes in the cluster (or we don't know about them)
+        if (peers.size() <= 0)
+            return;
+
+        logger.info("choosing to block until {}% of peers are marked alive and connections are established; max time to wait = {} seconds",
+                    targetPercent, timeoutSecs);
+
+        // first, send out a ping message to open up the non-gossip connections
+        final AtomicInteger connectedCount = sendPingMessages(peers);
+
+        final long startNanos = System.nanoTime();
+        final long expirationNanos = startNanos + TimeUnit.SECONDS.toNanos(timeoutSecs);
+        int completedRounds = 0;
+        while (checkStatus(peers, connectedCount, startNanos, expirationNanos < System.nanoTime(), completedRounds) == State.CONTINUE)
+        {
+            completedRounds++;
+            Uninterruptibles.sleepUninterruptibly(1, TimeUnit.MICROSECONDS);
+        }
+    }
+
+    State checkStatus(Set<InetAddress> peers, AtomicInteger connectedCount, final long startNanos, boolean beyondExpiration, final int completedRounds)
+    {
+        long currentAlive = peers.stream().filter(gossipStatus).count();
+        float currentAlivePercent = ((float) currentAlive / (float) peers.size()) * 100;
+
+        // assume two connections to remote host that we care to track here (small msg & large msg)
+        final int totalConnectionsSize = peers.size() * 2;
+        final int connectionsCount = connectedCount.get();
+        float currentConnectedPercent = ((float) connectionsCount / (float) totalConnectionsSize) * 100;
+
+        if (currentAlivePercent >= targetPercent && currentConnectedPercent >= targetPercent)
+        {
+            logger.info("after {} milliseconds, found {}% ({} / {}) of peers as marked alive, " +
+                        "and {}% ({} / {}) of peers as connected, " +
+                        "both of which are above the desired threshold of {}%",
+                        TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos),
+                        currentAlivePercent, currentAlive, peers.size(),
+                        currentConnectedPercent, connectionsCount, totalConnectionsSize,
+                        targetPercent);
+            return State.FINISH_SUCCESS;
+        }
+
+        // perform at least two rounds of checking, else this is kinda useless (and the operator set the aliveTimeoutSecs too low)
+        if (completedRounds >= 2 && beyondExpiration)
+        {
+            logger.info("after {} milliseconds, found {}% ({} / {}) of peers as marked alive, " +
+                        "and {}% ({} / {}) of peers as connected, " +
+                        "one or both of which is below the desired threshold of {}%",
+                        TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startNanos),
+                        currentAlivePercent, currentAlive, peers.size(),
+                        currentConnectedPercent, connectionsCount, totalConnectionsSize,
+                        targetPercent);
+            return State.FINISH_TIMEOUT;
+        }
+        return State.CONTINUE;
+    }
+
+    /**
+     * Sends a "connection warmup" message to each peer in the collection, on every {@link OutboundConnectionIdentifier.ConnectionType}
+     * used for internode messaging.
+     */
+    private AtomicInteger sendPingMessages(Set<InetAddress> peers)
+    {
+        AtomicInteger connectedCount = new AtomicInteger(0);
+        IAsyncCallback responseHandler = new IAsyncCallback()
+        {
+            @Override
+            public boolean isLatencyForSnitch()
+            {
+                return false;
+            }
+
+            @Override
+            public void response(MessageIn msg)
+            {
+                connectedCount.incrementAndGet();
+            }
+        };
+
+        MessageOut<PingMessage> smallChannelMessageOut = new MessageOut<>(PING, PingMessage.smallChannelMessage, PingMessage.serializer);
+        MessageOut<PingMessage> largeChannelMessageOut = new MessageOut<>(PING, PingMessage.largeChannelMessage, PingMessage.serializer);
+        for (InetAddress peer : peers)
+        {
+            MessagingService.instance().sendRR(smallChannelMessageOut, peer, responseHandler);
+            MessagingService.instance().sendRR(largeChannelMessageOut, peer, responseHandler);
+        }
+
+        return connectedCount;
+    }
+}

test/unit/org/apache/cassandra/net/StartupClusterConnectivityCheckerTest.java
@@ -1,0 +1,128 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.net;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
+
+import com.google.common.net.InetAddresses;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StartupClusterConnectivityCheckerTest
+{
+    @Test
+    public void testConnectivity_SimpleHappyPath() throws UnknownHostException
+    {
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(70, 10, addr -> true);
+        int count = 10;
+        Set<InetAddress> peers = createNodes(count);
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.FINISH_SUCCESS,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(count * 2), System.nanoTime(), false, 0));
+    }
+
+    @Test
+    public void testConnectivity_SimpleContinue() throws UnknownHostException
+    {
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(70, 10, addr -> true);
+        int count = 10;
+        Set<InetAddress> peers = createNodes(count);
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.CONTINUE,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(0), System.nanoTime(), false, 0));
+    }
+
+    @Test
+    public void testConnectivity_Timeout() throws UnknownHostException
+    {
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(70, 10, addr -> true);
+        int count = 10;
+        Set<InetAddress> peers = createNodes(count);
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.CONTINUE,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(0), System.nanoTime(), false, 4));
+        Assert.assertEquals(StartupClusterConnectivityChecker.State.FINISH_TIMEOUT,
+                            connectivityChecker.checkStatus(peers, new AtomicInteger(0), System.nanoTime(), true, 5));
+    }
+
+    @Test
+    public void testConnectivity_SimpleUpdating() throws UnknownHostException
+    {
+        UpdatablePredicate predicate = new UpdatablePredicate();
+        final int count = 100;
+        final int thresholdPercentage = 70;
+        StartupClusterConnectivityChecker connectivityChecker = new StartupClusterConnectivityChecker(thresholdPercentage, 10, predicate);
+        Set<InetAddress> peers = createNodes(count);
+
+        AtomicInteger connectedCount = new AtomicInteger();
+
+        for (int i = 0; i < count; i++)
+        {
+            predicate.reset(i);
+            connectedCount.set(i * 2);
+            StartupClusterConnectivityChecker.State expectedState = i < thresholdPercentage ?
+                                                                    StartupClusterConnectivityChecker.State.CONTINUE :
+                                                                    StartupClusterConnectivityChecker.State.FINISH_SUCCESS;
+            Assert.assertEquals("failed on iteration " + i,
+                                expectedState, connectivityChecker.checkStatus(peers, connectedCount, System.nanoTime(), false, i));
+        }
+    }
+
+    /**
+     * returns true until index = threshold, then returns false.
+     */
+    private class UpdatablePredicate implements Predicate<InetAddress>
+    {
+        int index;
+        int threshold;
+
+        void reset(int threshold)
+        {
+            index = 0;
+            this.threshold = threshold;
+        }
+
+        @Override
+        public boolean test(InetAddress inetAddress)
+        {
+            index++;
+            return index <= threshold;
+        }
+    }
+
+    private static Set<InetAddress> createNodes(int count) throws UnknownHostException
+    {
+        Set<InetAddress> nodes = new HashSet<>();
+
+        if (count < 1)
+            Assert.fail("need at least *one* node in the set!");
+
+        InetAddress node = InetAddress.getByName("127.0.0.1");
+        nodes.add(node);
+        for (int i = 1; i < count; i++)
+        {
+            node = InetAddress.getByAddress(InetAddresses.increment(node).getAddress());
+            nodes.add(node);
+        }
+        return nodes;
+    }
+
+}