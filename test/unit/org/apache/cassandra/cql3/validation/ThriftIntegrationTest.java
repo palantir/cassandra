8ddd87620f399fb717d80b23c45ff495c62eb28d
Zac Pear <zpear@palantir.com>
2024-08-16 14:03:57 -0400

    test bad behavior

--- test/unit/org/apache/cassandra/cql3/validation/ThriftIntegrationTest.java
+++ test/unit/org/apache/cassandra/cql3/validation/ThriftIntegrationTest.java
@@ -158,6 +158,11 @@ public class ThriftIntegrationTest extends ThriftCQLTester
     }
 
     @Test
+    public void testShutdown() throws Throwable {
+
+    }
+
+    @Test
     public void testCounterTableCqlUpdates() throws Throwable
     {
         populateCounterTable();
