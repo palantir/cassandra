af6d3e84a62a9945ccc04e15d681a1c3474cdc4e
Raymond Huffman <rhuffy@me.com>
2024-08-22 13:46:55 -0400

    Revert "Persist isCompacted status of SSTables and scrub on startup (#521)"
    This reverts commit 51522bdc1b9f8e0bcbb41c4512bc0a6210727974.

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -613,12 +613,6 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
                 SSTable.delete(desc, components);
                 continue;
             }
-            if (components.contains(Component.OBSOLETE))
-            {
-                logger.warn("Removing obsolete (compacted into new SSTable but not deleted) SSTable {}", desc);
-                SSTable.delete(desc, components);
-                continue;
-            }
 
             File dataFile = new File(desc.filenameFor(Component.DATA));
             if (components.contains(Component.DATA) && dataFile.length() > 0)
@@ -843,8 +837,6 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
             if (currentDescriptors.contains(descriptor))
                 continue; // old (initialized) SSTable found, skipping
             if (descriptor.type.isTemporary) // in the process of being written
-                continue;
-            if (entry.getValue().contains(Component.OBSOLETE))
                 continue;
 
             if (!descriptor.isCompatible())
