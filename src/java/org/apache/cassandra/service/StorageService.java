1f058ae61d1b4b882a59df3b7ad5793286635bce
Raymond Huffman <rhuffman@palantir.com>
2024-10-28 17:48:29 -0400

    whitespace

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -44,7 +44,6 @@ import com.palantir.cassandra.settings.LocalQuorumReadForSerialCasSetting;
 import com.palantir.logsafe.Safe;
 import com.palantir.logsafe.SafeArg;
 import org.apache.cassandra.schema.LegacySchemaTables;
-
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
