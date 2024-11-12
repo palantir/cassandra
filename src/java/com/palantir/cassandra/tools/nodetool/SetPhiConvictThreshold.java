e6c5da9ce9e702649cee5652eaebbcfe802b6815
inespot <34454302+inespot@users.noreply.github.com>
2024-11-12 17:14:17 -0500

    Phi threshold can be set via nodetool (#579)
    Co-authored-by: ines potier <ipotier@palantir.com>

---
+++ src/java/com/palantir/cassandra/tools/nodetool/SetPhiConvictThreshold.java
@@ -0,0 +1,39 @@
+/*
+ * (c) Copyright 2024 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.cassandra.tools.nodetool;
+
+import org.apache.cassandra.tools.NodeProbe;
+import org.apache.cassandra.tools.NodeTool;
+
+import io.airlift.command.Arguments;
+import io.airlift.command.Command;
+
+@Command(name = "setphiconvictthreshold", description = "Set failure detector phi threshold")
+public class SetPhiConvictThreshold extends NodeTool.NodeToolCmd
+{
+    @Arguments(
+    title = "phi_convict_treshold",
+    usage = "<value>",
+    description = "Phi value",
+    required = true)
+    private Double threshold = null;
+
+    @Override
+    protected void execute(NodeProbe probe) {
+        probe.setPhiConvictThreshold(threshold);
+    }
+}
