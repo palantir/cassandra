68e47fdf395fc6525c33addb6d65058b078d7e89
Zac Pear <zpear@palantir.com>
2024-11-21 10:16:46 -0500

    wip

--- test/unit/org/apache/cassandra/net/MessagingServiceTest.java
+++ test/unit/org/apache/cassandra/net/MessagingServiceTest.java
@@ -20,15 +20,54 @@
  */
 package org.apache.cassandra.net;
 
+import java.net.InetAddress;
+import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import com.google.common.collect.ImmutableList;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+import org.apache.cassandra.SchemaLoader;
+import org.apache.cassandra.Util;
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.config.KSMetaData;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.Keyspace;
+import org.apache.cassandra.db.Mutation;
+import org.apache.cassandra.db.SystemKeyspace;
+import org.apache.cassandra.db.WriteType;
+import org.apache.cassandra.db.marshal.UUIDType;
+import org.apache.cassandra.exceptions.ConfigurationException;
+import org.apache.cassandra.locator.SimpleStrategy;
+import org.apache.cassandra.service.AbstractWriteResponseHandler;
+import org.apache.cassandra.service.WriteResponseHandler;
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.FBUtilities;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class MessagingServiceTest
 {
+    private static final String KEYSPACE1 = "MessagingServiceKeyspace";
+    private static final String CF_STANDARD1 = "columnfamily";
     private final MessagingService messagingService = MessagingService.test();
+
+    @BeforeClass
+    public static void defineSchema() throws ConfigurationException
+    {
+        SchemaLoader.prepareServer();
+        SchemaLoader.createKeyspace(KEYSPACE1,
+                                    SimpleStrategy.class,
+                                    KSMetaData.optsWithRF(1),
+                                    SchemaLoader.standardCFMD(KEYSPACE1, CF_STANDARD1));
+    }
 
     @Test
     public void testDroppedMessages()
@@ -55,11 +94,66 @@ public class MessagingServiceTest
     }
 
     @Test
-    public void shutdown_refusesNewMessagesWhenInProgress() {
+    public void shutdown_refusesNewMessagesWhenInProgress() throws InterruptedException
+    {
+
+        Keyspace keyspace = Keyspace.open(KEYSPACE1);
+        //ColumnFamilyStore store = keyspace.getColumnFamilyStore(CF_STANDARD1);
+        DecoratedKey dk = Util.dk("key1");
+
+        // add data
+        Mutation mutation = new Mutation(KEYSPACE1, dk.getKey());
+        mutation.add(CF_STANDARD1, Util.cellname("Column1"), ByteBufferUtil.bytes("asdf"), 0);
+        //mutation.applyUnsafe();
+        AtomicBoolean response = new AtomicBoolean(false);
+        TestHandler handler = new TestHandler(ImmutableList.of(FBUtilities.getLocalAddress()), null, ConsistencyLevel.ANY, keyspace, () -> response.set(true), WriteType.SIMPLE);
+        MessageOut<Mutation> message = mutation.createMessage();
+        try
+        {
+            messagingService.listen();
+
+            MessagingService.instance().sendRR(message, FBUtilities.getLocalAddress(), handler, false);
+            Thread.sleep(1000);
+            //MessagingService.instance().receive();
+//            while (true)
+//            {
+//                try
+//                {
+//                    Thread.sleep(500);
+//                }
+//                catch (InterruptedException ignored)
+//                {
+//                    break;
+//                }
+//            }
+        }finally
+        {
+            messagingService.shutdown();
+        }
+        assertTrue(response.get());
+
+        MessagingService.instance().sendRR(message, FBUtilities.getLocalAddress(), handler, false);
+        Thread.sleep(1000);
+        assertTrue(response.get());
+
         // Create incoming tcp connection
         // call shutdown
         // send something over the connection before shutdown completes
         // assert that socket was closed when we are reordered
 
     }
+
+        static class TestHandler<T> extends WriteResponseHandler<T> {
+
+            public TestHandler(Collection<InetAddress> writeEndpoints, Collection<InetAddress> pendingEndpoints, ConsistencyLevel consistencyLevel, Keyspace keyspace, Runnable callback, WriteType writeType)
+            {
+                super(writeEndpoints, pendingEndpoints, consistencyLevel, keyspace, callback, writeType);
+            }
+
+            @Override
+            protected int totalBlockFor() {
+                return 1;
+            }
+        }
+
 }
