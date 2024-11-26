b21a63afe33db893f7a3b0c522ad0d75440a6b6d
Zac Pear <zpear@palantir.com>
2024-11-26 14:54:38 -0500

    print statements

--- src/java/org/apache/cassandra/net/OutboundTcpConnection.java
+++ src/java/org/apache/cassandra/net/OutboundTcpConnection.java
@@ -281,6 +281,7 @@ public class OutboundTcpConnection extends Thread
     {
         try
         {
+            System.out.println("Writing " + qm.id);
             byte[] sessionBytes = qm.message.parameters.get(Tracing.TRACE_HEADER);
             if (sessionBytes != null)
             {
@@ -308,6 +309,7 @@ public class OutboundTcpConnection extends Thread
             completed++;
             if (flush)
                 out.flush();
+            System.out.println("Successfully wrote " + qm.id);
         }
         catch (Throwable e)
         {
@@ -333,6 +335,7 @@ public class OutboundTcpConnection extends Thread
                 } else {
                     CallbackInfo registeredCallbackInfo = MessagingService.instance().getRegisteredCallback(qm.id);
                     if (registeredCallbackInfo != null && registeredCallbackInfo.isFailureCallback()) {
+                        System.out.println("Failed " + qm.id);
                         ((IAsyncCallbackWithFailure) MessagingService.instance().removeRegisteredCallback(qm.id).callback).onFailure(poolReference.endPoint());
                     }
                 }
