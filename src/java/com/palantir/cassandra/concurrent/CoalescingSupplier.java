8312101ca840f03a8665e0071444296635889e31
Daniel Guo <danielguo@palantir.com>
2024-12-02 20:27:39 -0500

    oops

--- src/java/com/palantir/cassandra/concurrent/CoalescingSupplier.java
+++ src/java/com/palantir/cassandra/concurrent/CoalescingSupplier.java
@@ -1,11 +1,9 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -26,11 +24,9 @@ import java.util.function.Supplier;
 import com.google.common.base.Throwables;
 
 /**
- * (c) Copyright 2019 Palantir Technologies Inc. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * A supplier that coalesces computation requests, such that only one computation is ever running at a time, and
+ * concurrent requests will result in a single computation. Computations are guaranteed to execute after being
+ * requested; requests will not receive results for computations that started prior to the request.
  * 
  * Copied directly from:
  * https://github.com/palantir/atlasdb/blob/0.138.0/atlasdb-commons/src/main/java/com/palantir/common/concurrent/CoalescingSupplier.java
