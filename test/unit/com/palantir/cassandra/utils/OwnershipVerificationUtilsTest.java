f91acc39d31ab96c3aeb537c25bac949d75c24de
Raymond Huffman <rhuffman@palantir.com>
2025-01-31 14:46:49 -0500

    fix compile

--- test/unit/com/palantir/cassandra/utils/OwnershipVerificationUtilsTest.java
+++ test/unit/com/palantir/cassandra/utils/OwnershipVerificationUtilsTest.java
@@ -24,7 +24,6 @@ import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.exceptions.ConfigurationException;
-import org.apache.cassandra.exceptions.InvalidMutationException;
 import org.apache.cassandra.locator.NetworkTopologyStrategy;
 import org.apache.cassandra.locator.TokenMetadata;
 import org.apache.cassandra.service.StorageService;
