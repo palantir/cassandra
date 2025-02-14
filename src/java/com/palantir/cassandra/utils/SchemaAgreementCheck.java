7fd48fa2ebdb6cce69dc8ca86de05bac39333485
ines potier <ipotier@palantir.com>
2025-02-13 21:33:07 -0500

    Fix typo

--- src/java/com/palantir/cassandra/utils/SchemaAgreementCheck.java
+++ src/java/com/palantir/cassandra/utils/SchemaAgreementCheck.java
@@ -60,7 +60,7 @@ public class SchemaAgreementCheck
     {
         UUID localSchemaVersion = localSchemaVersionSupplier.get();
         return endpointStatesSupplier.get().stream()
-                                     .filter(endpoint -> nodesToSkip.contains(endpoint.getKey()))
+                                     .filter(endpoint -> !nodesToSkip.contains(endpoint.getKey()))
                                      .filter(endpointStates -> !isLeft(endpointStates.getValue()))
                                      .allMatch(endpointStates -> schemaIsEqualToLocalVersion(localSchemaVersion, endpointStates.getKey(), endpointStates.getValue()));
     }
