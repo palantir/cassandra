be24014c9e136d47e2e723fe87ff3d05b940e5a8
Leon Cheung <lcheung@palantir.com>
2025-02-19 15:09:15 -0500

    Revert "add test"
    This reverts commit c26e8f3bb56c640d820da48131115f7e157b0ead.

--- src/java/org/apache/cassandra/repair/RepairJob.java
+++ src/java/org/apache/cassandra/repair/RepairJob.java
@@ -21,7 +21,6 @@ import java.net.InetAddress;
 import java.util.*;
 
 import com.google.common.util.concurrent.*;
-import com.palantir.logsafe.Arg;
 import com.palantir.logsafe.SafeArg;
 
 import org.slf4j.Logger;
@@ -151,7 +150,6 @@ public class RepairJob extends AbstractFuture<RepairResult> implements Runnable
             public void onSuccess(List<SyncStat> stats)
             {
                 logger.info(String.format("[repair #{}] {} is fully synced", SafeArg.of("sessionId", session.getId()), SafeArg.of("columnFamily", desc.columnFamily)));
-                logErrorTest("LCHEUNG LOG TEST {} {}", SafeArg.of("sessionId", session.getId()), SafeArg.of("columnFamily", desc.columnFamily));
                 SystemDistributedKeyspace.successfulRepairJob(session.getId(), desc.keyspace, desc.columnFamily);
                 set(new RepairResult(desc, stats));
             }
@@ -169,10 +167,6 @@ public class RepairJob extends AbstractFuture<RepairResult> implements Runnable
 
         // Wait for validation to complete
         Futures.getUnchecked(validations);
-    }
-
-    private static void logErrorTest(String errorMessageFmt, Arg<?>... args) {
-        logger.error(errorMessageFmt, (Object[]) args);
     }
 
     /**
