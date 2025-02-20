1747e3e00678f8189d82c4783f55fc05ce2ea239
Raymond Huffman <rhuffman@palantir.com>
2025-02-20 06:54:10 -0500

    fix small mistakes from merge

--- test/unit/org/apache/cassandra/dht/BootStrapperTest.java
+++ test/unit/org/apache/cassandra/dht/BootStrapperTest.java
@@ -196,7 +196,7 @@ public class BootStrapperTest
             int vn = 16;
             String ks = "BootStrapperTestNTSKeyspace" + rackCount + replicas;
             String dc = "1";
-            SchemaLoader.createKeyspace(ks, NetworkTopologyStrategy.class, ImmutableMap.of(dc, "15"), SchemaLoader.standardCFMD(ks, "Standard1"));
+            SchemaLoader.createKeyspace(ks, NetworkTopologyStrategy.class, ImmutableMap.of(dc, String.valueOf(replicas)), SchemaLoader.standardCFMD(ks, "Standard1"));
             TokenMetadata tm = StorageService.instance.getTokenMetadata();
             tm.clearUnsafe();
             for (int i = 0; i < rackCount; ++i)
@@ -283,7 +283,7 @@ public class BootStrapperTest
             metadata.updateHostId(UUID.randomUUID(), InetAddress.getByName("127.1.0.99"));
             metadata.updateHostId(UUID.randomUUID(), InetAddress.getByName("127.15.0.99"));
 
-            SchemaLoader.createKeyspace(ks, NetworkTopologyStrategy.class, ImmutableMap.of(dc, "15"), SchemaLoader.standardCFMD(ks, "Standard1"));
+            SchemaLoader.createKeyspace(ks, NetworkTopologyStrategy.class, ImmutableMap.of(dc, String.valueOf(replicas)), SchemaLoader.standardCFMD(ks, "Standard1"));
             int base = 5;
             for (int i = 0; i < rackCount; ++i)
                 generateFakeEndpoints(metadata, base << i, vn, dc, Integer.toString(i));     // unbalanced racks
