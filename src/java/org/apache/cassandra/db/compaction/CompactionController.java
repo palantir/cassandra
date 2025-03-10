c3cc368785d0dcd48e3098714c19a49221def7d0
Raymond Huffman <rhuffman@palantir.com>
2025-03-10 18:11:06 -0400

    do not purge tombstones on any cfs being repaired

--- src/java/org/apache/cassandra/db/compaction/CompactionController.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionController.java
@@ -34,6 +34,7 @@ import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.db.Memtable;
 import org.apache.cassandra.db.RowPosition;
+import org.apache.cassandra.service.ActiveRepairService;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.utils.AlwaysPresentFilter;
 
@@ -79,7 +80,7 @@ public class CompactionController implements AutoCloseable
 
     void maybeRefreshOverlaps()
     {
-        if (doNotPurgeTombstones(cfs.keyspace.getName()))
+        if (doNotPurgeTombstones(cfs))
         {
             logger.debug("not refreshing overlaps - doNotPurgeTombstones returned true for keyspace {}",
                          SafeArg.of("keyspace", cfs.keyspace.getName()));
@@ -98,7 +99,7 @@ public class CompactionController implements AutoCloseable
 
     private void refreshOverlaps()
     {
-        if (doNotPurgeTombstones(cfs.keyspace.getName()))
+        if (doNotPurgeTombstones(cfs))
         {
             logger.debug("not refreshing overlaps - doNotPurgeTombstones returned true for keyspace {}",
                          SafeArg.of("keyspace", cfs.keyspace.getName()));
@@ -145,9 +146,9 @@ public class CompactionController implements AutoCloseable
         if (compacting == null)
             return Collections.<SSTableReader>emptySet();
 
-        if (doNotPurgeTombstones(cfStore.keyspace.getName())) {
-            logger.debug("not looking for droppable sstables - doNotPurgeTombstones returned true for keyspace {}",
-                         SafeArg.of("keyspace", cfStore.keyspace.getName()));
+        if (doNotPurgeTombstones(cfStore))
+        {
+            logger.debug("Not looking for droppable sstables - doNotPurgeTombstones returned true for keyspace {}", cfStore.keyspace.getName());
             return Collections.<SSTableReader>emptySet();
         }
 
@@ -219,7 +220,7 @@ public class CompactionController implements AutoCloseable
      */
     public Predicate<Long> getPurgeEvaluator(DecoratedKey key)
     {
-        if (doNotPurgeTombstones(getKeyspace()))
+        if (doNotPurgeTombstones(cfs))
         {
             logger.debug("Purge evaluator always returning false - doNotPurgeTombstones returned true for keyspace {}",
                          SafeArg.of("keyspace", getKeyspace()));
@@ -278,17 +279,18 @@ public class CompactionController implements AutoCloseable
     }
 
     /**
-     * @param keyspace
+     * @param cfs
      * @return true if this node may be streaming data for this keyspace, or if cassandra.never_purge_tombstones is set
      * If we are streaming, tombstones should not be purged so that we don't pre-maturely purge those that exceed gc_grace_seconds.
      * <p>
      * Note that this node may receive data without streaming, e.g. during a repair.
      */
-    private static boolean doNotPurgeTombstones(String keyspace)
+    private static boolean doNotPurgeTombstones(ColumnFamilyStore cfs)
     {
         return NEVER_PURGE_TOMBSTONES
                 || StorageService.instance.isRebuilding()
-                || pendingRangesExistForKeyspace(keyspace);
+                || pendingRangesExistForKeyspace(cfs.keyspace.getName())
+                || ActiveRepairService.instance.isRepairing(cfs.metadata.cfId);
     }
 
     /**
