ed2148501f0f03e64ec49da3d5c2acb6a3670111
Raymond Huffman <rhuffman@palantir.com>
2025-03-24 16:39:09 -0400

    use DD for SS#isNewCluster

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -5427,7 +5427,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     @Override
     public boolean isNewCluster()
     {
-        return Boolean.parseBoolean(System.getProperty("palantir_cassandra.is_new_cluster", "false"));
+        return DatabaseDescriptor.getIsNewCluster();
     }
 
     @Override
