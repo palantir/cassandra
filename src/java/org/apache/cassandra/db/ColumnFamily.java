b5e6a487cc64f5eb73071b7bafaedd7aa5a5864d
Daniel Guo <danielguo@palantir.com>
2025-03-25 16:51:14 -0400

    rename to deconflict

--- src/java/org/apache/cassandra/db/ColumnFamily.java
+++ src/java/org/apache/cassandra/db/ColumnFamily.java
@@ -407,7 +407,7 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
         return sb.toString();
     }
 
-    public static ByteBuffer digestWithPageToken(ColumnFamily cf, PageToken pageToken)
+    public static ByteBuffer digestLimitByPageToken(ColumnFamily cf, PageToken pageToken)
     {
         MessageDigest digest = FBUtilities.threadLocalMD5Digest();
         if (cf != null)
@@ -418,13 +418,13 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
             }
             else
             {
-                cf.updateDigestWithPageToken(digest, pageToken);
+                cf.updateDigestLimitByPageToken(digest, pageToken);
             }
         }
         return ByteBuffer.wrap(digest.digest());
     }
 
-    public void updateDigestWithPageToken(MessageDigest digest, PageToken pageToken)
+    public void updateDigestLimitByPageToken(MessageDigest digest, PageToken pageToken)
     {
         assert !pageToken.isReachedEnd();
         
