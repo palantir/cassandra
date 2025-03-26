9e951b73a0e9657ed0aa904d86cb3a0d24fd5946
Daniel Guo <dguo2000@yahoo.com>
2025-03-26 11:35:22 -0400

    Delay JMX Initialization (#646)
    Co-authored-by: Daniel Guo <danielguo@palantir.com>

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -224,12 +224,14 @@ public class CassandraDaemon
         // This should be the first write to SystemKeyspace (CASSANDRA-11742)
         SystemKeyspace.persistLocalMetadata();
 
-        maybeInitJmx();
-
         Directories.scheduleVerifyingDiskDoesNotExceedThresholdChecks();
 
         doNotStartupClientInterfacesIfDisabled();
-        completeSetupMayThrowSstableException();
+        beginSetupMayThrowSstableException();
+        // It is important to only initialize JMX at this point because `beginSetupMayThrowSstableException` does compaction product cleanup,
+        // which must happen after sstable views have been initialized. Some JMX endpoints could otherwise intialize the views prematurely.
+        maybeInitJmx();
+        recoverCommitlogAndCompleteSetup();
 
         logger.debug("Completed CassandraDaemon setup.");
     }
@@ -261,7 +263,8 @@ public class CassandraDaemon
     }
 
     /* This part of setup may throw a CorruptSSTableException. */
-    private void completeSetupMayThrowSstableException() {
+    private void beginSetupMayThrowSstableException()
+    {
         // load schema from disk
         Schema.instance.loadFromDisk();
 
@@ -335,8 +338,6 @@ public class CassandraDaemon
             JVMStabilityInspector.inspectThrowable(t);
             logger.warn("Unable to start GCInspector (currently only supported on the Sun JVM)");
         }
-
-        recoverCommitlogAndCompleteSetup();
     }
 
     private void recoverCommitlogAndCompleteSetup() {
@@ -813,7 +814,8 @@ public class CassandraDaemon
             if (!CassandraDaemon.instance.setupCompleted())
             {
                 // if setup wasn't completed, then an FS error occurred early; we should re-attempt
-                CassandraDaemon.instance.completeSetupMayThrowSstableException();
+                CassandraDaemon.instance.beginSetupMayThrowSstableException();
+                CassandraDaemon.instance.recoverCommitlogAndCompleteSetup();
             }
             else
             {
