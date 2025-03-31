55f01689859d059e6d0daf5e54df85f532acd8d5
Daniel Guo <danielguo@palantir.com>
2025-03-31 09:55:22 -0400

    first tests

--- src/java/org/apache/cassandra/db/ColumnFamily.java
+++ src/java/org/apache/cassandra/db/ColumnFamily.java
@@ -21,12 +21,7 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 import com.google.common.collect.ImmutableMap;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
@@ -435,10 +430,12 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
             return false;
 
         ColumnFamily comparison = (ColumnFamily) o;
+        System.out.println("page token here: ");
+        System.out.println(pageToken);
 
         return metadata.equals(comparison.metadata)
                && deletionInfo().equals(comparison.deletionInfo())
-               && ByteBufferUtil.compareUnsigned(digest(this), digest(comparison)) == 0;
+                && ByteBufferUtil.compareUnsigned(digest(this), digest(comparison)) == 0 && Objects.equals(pageToken, comparison.pageToken);
     }
 
     @Override
