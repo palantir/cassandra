c2dacb0b2518b556f1a41d110f0f8688aa7bbd06
Daniel Guo <danielguo@palantir.com>
2025-03-31 13:12:20 -0400

    more tests

--- src/java/org/apache/cassandra/db/ColumnFamily.java
+++ src/java/org/apache/cassandra/db/ColumnFamily.java
@@ -100,7 +100,6 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
 
     public ColumnFamily cloneLimitByPageToken(PageToken pageToken)
     {
-        assert pageToken != null;
         ColumnFamily cf = cloneMeShallow();
         cf.delete(this);
 
@@ -111,7 +110,7 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
         {
             Cell cell = iter.next();
 
-            if (comparator.compare(cell.name(), pageToken.getCell().name()) < 0)
+            if (pageToken == null || pageToken.isReachedEnd() || comparator.compare(cell.name(), pageToken.getCell().name()) < 0)
             {
                 cf.addColumn(cell);
             }
@@ -120,7 +119,10 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
                 break;
             }
         }
-        cf.setPageToken(pageToken);
+        if (pageToken != null)
+        {
+            cf.setPageToken(pageToken);
+        }
 
         return cf;
     }
@@ -242,7 +244,8 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
 
     public abstract void delete(DeletionInfo info);
     public abstract void delete(DeletionTime deletionTime);
-    protected abstract void delete(RangeTombstone tombstone);
+
+    public abstract void delete(RangeTombstone tombstone);
 
     public abstract SearchIterator<CellName, Cell> searchIterator();
 
