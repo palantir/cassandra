65141bfb1d51bb47e5f186842d10bbcfc4257463
Nick Doan <hdoan@palantir.com>
2025-04-01 16:15:38 -0400

    Fix silly mistakr

--- src/java/org/apache/cassandra/service/RangeSliceResponseResolver.java
+++ src/java/org/apache/cassandra/service/RangeSliceResponseResolver.java
@@ -42,14 +42,11 @@ import org.apache.cassandra.utils.Pair;
  * Turns RangeSliceReply objects into row (string -> CF) maps, resolving
  * to the most recent ColumnFamily and setting up read repairs as necessary.
  */
-public class RangeSliceResponseResolver implements IResponseResolver<RangeSliceReply, Iterable<Row>>
-{
+public class RangeSliceResponseResolver implements IResponseResolver<RangeSliceReply, Iterable<Row>> {
     private static final Logger logger = LoggerFactory.getLogger(RangeSliceResponseResolver.class);
 
-    private static final Comparator<Pair<Row,InetAddress>> pairComparator = new Comparator<Pair<Row, InetAddress>>()
-    {
-        public int compare(Pair<Row, InetAddress> o1, Pair<Row, InetAddress> o2)
-        {
+    private static final Comparator<Pair<Row, InetAddress>> pairComparator = new Comparator<Pair<Row, InetAddress>>() {
+        public int compare(Pair<Row, InetAddress> o1, Pair<Row, InetAddress> o2) {
             return o1.left.key.compareTo(o2.left.key);
         }
     };
@@ -60,36 +57,34 @@ public class RangeSliceResponseResolver implements IResponseResolver<RangeSliceR
     protected final Queue<MessageIn<RangeSliceReply>> responses = new ConcurrentLinkedQueue<>();
     public final List<AsyncOneResponse> repairResults = new ArrayList<>();
 
-    public RangeSliceResponseResolver(String keyspaceName, long timestamp)
-    {
+    public RangeSliceResponseResolver(String keyspaceName, long timestamp) {
         this.keyspaceName = keyspaceName;
         this.timestamp = timestamp;
     }
 
-    public void setSources(List<InetAddress> endpoints)
-    {
+    public void setSources(List<InetAddress> endpoints) {
         this.sources = endpoints;
     }
 
-    public List<Row> getData()
-    {
+    public List<Row> getData() {
         assert !responses.isEmpty();
         return responses.peek().payload.rows;
     }
 
-    // Note: this would deserialize the response a 2nd time if getData was called first.
-    // (this is not currently an issue since we don't do read repair for range queries.)
-    public Iterable<Row> resolve()
-    {
+    // Note: this would deserialize the response a 2nd time if getData was called
+    // first.
+    // (this is not currently an issue since we don't do read repair for range
+    // queries.)
+    public Iterable<Row> resolve() {
         ArrayList<RowIterator> iters = new ArrayList<>(responses.size());
         int n = 0;
-        for (MessageIn<RangeSliceReply> response : responses)
-        {
+        for (MessageIn<RangeSliceReply> response : responses) {
             RangeSliceReply reply = response.payload;
             n = Math.max(n, reply.rows.size());
             iters.add(new RowIterator(reply.rows.iterator(), response.from));
         }
-        // for each row, compute the combination of all different versions seen, and repair incomplete versions
+        // for each row, compute the combination of all different versions seen, and
+        // repair incomplete versions
         // TODO do we need to call close?
         CloseableIterator<Row> iter = MergeIterator.get(iters, pairComparator, new Reducer());
 
@@ -100,82 +95,76 @@ public class RangeSliceResponseResolver implements IResponseResolver<RangeSliceR
         return resolvedRows;
     }
 
-    public void preprocess(MessageIn message)
-    {
+    public void preprocess(MessageIn message) {
         responses.add(message);
     }
 
-    public boolean isDataPresent()
-    {
+    public boolean isDataPresent() {
         return !responses.isEmpty();
     }
 
-    private static class RowIterator extends AbstractIterator<Pair<Row,InetAddress>> implements CloseableIterator<Pair<Row,InetAddress>>
-    {
+    private static class RowIterator extends AbstractIterator<Pair<Row, InetAddress>>
+            implements CloseableIterator<Pair<Row, InetAddress>> {
         private final Iterator<Row> iter;
         private final InetAddress source;
 
-        private RowIterator(Iterator<Row> iter, InetAddress source)
-        {
+        private RowIterator(Iterator<Row> iter, InetAddress source) {
             this.iter = iter;
             this.source = source;
         }
 
-        protected Pair<Row,InetAddress> computeNext()
-        {
+        protected Pair<Row, InetAddress> computeNext() {
             return iter.hasNext() ? Pair.create(iter.next(), source) : endOfData();
         }
 
-        public void close() {}
+        public void close() {
+        }
     }
 
-    public Iterable<MessageIn<RangeSliceReply>> getMessages()
-    {
+    public Iterable<MessageIn<RangeSliceReply>> getMessages() {
         return responses;
     }
 
-    private class Reducer extends MergeIterator.Reducer<Pair<Row,InetAddress>, Row>
-    {
+    private class Reducer extends MergeIterator.Reducer<Pair<Row, InetAddress>, Row> {
         List<ColumnFamily> versions = new ArrayList<>(sources.size());
         List<InetAddress> versionSources = new ArrayList<InetAddress>(sources.size());
         DecoratedKey key;
 
-        public void reduce(Pair<Row,InetAddress> current)
-        {
+        public void reduce(Pair<Row, InetAddress> current) {
             key = current.left.key;
             versions.add(current.left.cf);
             versionSources.add(current.right);
         }
 
-        protected Row getReduced()
-        {
-            logger.info("Reduced {} {} {}", SafeArg.of("key", key.getToken()),  SafeArg.of("versions", versions.size()), SafeArg.of("sources", versionSources));
+        protected Row getReduced() {
+            logger.info("Reduced {} {} {}", SafeArg.of("key", key.getToken()), SafeArg.of("versions", versions.size()),
+                    SafeArg.of("sources", versionSources));
 
             ColumnFamily resolved = versions.size() > 1
-                                  ? RowDataResolver.resolveSuperset(versions, timestamp)
-                                  : versions.get(0);
-            if (versions.size() < sources.size())
-            {
-                // add placeholder rows for sources that didn't have any data, so maybeScheduleRepairs sees them
-                for (InetAddress source : sources)
-                {
-                    if (!versionSources.contains(source))
-                    {
+                    ? RowDataResolver.resolveSuperset(versions, timestamp)
+                    : versions.get(0);
+            if (versions.size() < sources.size()) {
+                // add placeholder rows for sources that didn't have any data, so
+                // maybeScheduleRepairs sees them
+                for (InetAddress source : sources) {
+                    if (!versionSources.contains(source)) {
                         versions.add(null);
                         versionSources.add(source);
                     }
                 }
             }
-            // resolved can be null even if versions doesn't have all nulls because of the call to removeDeleted in resolveSuperSet
+            // resolved can be null even if versions doesn't have all nulls because of the
+            // call to removeDeleted in resolveSuperSet
             if (resolved != null) {
-                logger.info("No resolved");
-                repairResults.addAll(RowDataResolver.scheduleRepairs(resolved, keyspaceName, key, versions, versionSources));
-            } else {
+                repairResults
+                        .addAll(RowDataResolver.scheduleRepairs(resolved, keyspaceName, key, versions, versionSources));
                 resolved.iterator().forEachRemaining(cell -> {
                     if (cell != null) {
                         logger.info("Cell {}", SafeArg.of("deletionTime", cell.getLocalDeletionTime()));
                     }
                 });
+            } else {
+                logger.info("No resolved");
             }
             versions.clear();
             versionSources.clear();
