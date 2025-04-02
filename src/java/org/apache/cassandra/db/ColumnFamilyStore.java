4700a2e879deebd578724ecae77884b647026b72
Daniel Guo <danielguo@palantir.com>
2025-04-02 17:11:17 -0400

    disallow compactions completely

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -3000,9 +3000,9 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
     }
 
 
-    public void forceMajorCompaction(boolean splitOutput) throws InterruptedException, ExecutionException
+    public void forceMajorCompaction(boolean _splitOutput) throws InterruptedException, ExecutionException
     {
-        CompactionManager.instance.performMaximal(this, splitOutput);
+//        CompactionManager.instance.performMaximal(this, splitOutput);
     }
 
     public static Iterable<ColumnFamilyStore> all()
