70318076afb54fea91297c0014682c6a2da04606
Daniel Guo <danielguo@palantir.com>
2025-04-03 23:49:33 -0400

    missing arg

--- src/java/org/apache/cassandra/db/SliceFromReadCommand.java
+++ src/java/org/apache/cassandra/db/SliceFromReadCommand.java
@@ -22,6 +22,8 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 
 import com.google.common.base.MoreObjects;
+import com.palantir.logsafe.SafeArg;
+import org.apache.cassandra.db.composites.Composite;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
