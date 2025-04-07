94e3d97adea39bac0f44fd38f34fb117c5c02251
Daniel Guo <danielguo@palantir.com>
2025-04-07 01:58:07 -0400

    Revert "Revert "remove logs on hot paths""
    This reverts commit f2a2192d9af66f24209785fdf281b3222cf64570.

--- src/java/org/apache/cassandra/db/SliceFromReadCommand.java
+++ src/java/org/apache/cassandra/db/SliceFromReadCommand.java
@@ -60,18 +60,6 @@ public class SliceFromReadCommand extends ReadCommand
 
     public Row getRow(Keyspace keyspace)
     {
-        if (keyspace.getName().equals("dg"))
-        {
-            Composite start = filter.start();
-            Composite finish = filter.finish();
-            StringBuilder sbStart = new StringBuilder();
-            Composite.toString(start.toByteBuffer(), sbStart);
-            StringBuilder sbFinish = new StringBuilder();
-            Composite.toString(finish.toByteBuffer(), sbFinish);
-            logger.info("Received read in verb handler", SafeArg.of("usePageToken", filter.usePageToken), SafeArg.of("start", sbStart.toString()), SafeArg.of("finish", sbFinish.toString()));
-        }
-
-
         CFMetaData cfm = Schema.instance.getCFMetaData(ksName, cfName);
         DecoratedKey dk = StorageService.getPartitioner().decorateKey(key);
 
@@ -95,21 +83,7 @@ public class SliceFromReadCommand extends ReadCommand
 
             return normalResults;
         }
-
-        Row row = keyspace.getRow(new QueryFilter(dk, cfName, filter, timestamp));
-        if (keyspace.getName().equals("dg") && row != null)
-        {
-            if (row.cf != null)
-            {
-                logger.info("Read done locally from verb handler", SafeArg.of("pageTokenSet", row.cf.isPageTokenSet()), SafeArg.of("pageToken", row.cf.pageToken()));
-            }
-            else
-            {
-                logger.info("Read done locally from verb handler but cf is null");
-            }
-        }
-
-        return row;
+        return keyspace.getRow(new QueryFilter(dk, cfName, filter, timestamp));
     }
 
     @Override
