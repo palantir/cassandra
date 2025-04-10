d2729e8787c869a131a8c0f516efdd0c4bfd8732
Daniel Guo <danielguo@palantir.com>
2025-04-10 14:05:45 -0400

    add get_range_slices_paging

--- src/java/org/apache/cassandra/thrift/ThriftValidation.java
+++ src/java/org/apache/cassandra/thrift/ThriftValidation.java
@@ -640,7 +640,7 @@ public class ThriftValidation
             throw new org.apache.cassandra.exceptions.InvalidRequestException("system keyspace is not user-modifiable");
     }
 
-    public static IDiskAtomFilter asIFilter(SlicePredicate sp, CFMetaData metadata, ByteBuffer superColumn)
+    public static IDiskAtomFilter asIFilter(SlicePredicate sp, CFMetaData metadata, ByteBuffer superColumn, boolean usePageToken)
     {
         SliceRange sr = sp.slice_range;
         IDiskAtomFilter filter;
@@ -658,10 +658,12 @@ public class ThriftValidation
         }
         else
         {
-            filter = new SliceQueryFilter(comparator.fromByteBuffer(sr.start),
-                                          comparator.fromByteBuffer(sr.finish),
-                                          sr.reversed,
-                                          sr.count);
+            filter = new SliceQueryFilter(
+                    comparator.fromByteBuffer(sr.start),
+                    comparator.fromByteBuffer(sr.finish),
+                    sr.reversed,
+                    usePageToken,
+                    sr.count);
         }
 
         if (metadata.isSuper())
