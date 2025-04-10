6af8b679d2cc854e6972eb7b4b66d7f9af8b96b9
Daniel Guo <danielguo@palantir.com>
2025-04-10 14:29:54 -0400

    Revert "small refactors and cleanups"
    This reverts commit 55f78499fef161d34362a08b508605e6e6cfc29f.

--- src/java/org/apache/cassandra/service/StartupChecks.java
+++ src/java/org/apache/cassandra/service/StartupChecks.java
@@ -201,6 +201,7 @@ public class StartupChecks
          */
         private void checkOutOfMemoryHandling() throws StartupException
         {
+            System.out.println(System.getProperty("java.version"));
             if (JavaUtils.supportExitOnOutOfMemory(System.getProperty("java.version")))
             {
                 if (!jvmOptionsContainsOneOf("-XX:OnOutOfMemoryError=", "-XX:+ExitOnOutOfMemoryError", "-XX:+CrashOnOutOfMemoryError"))
