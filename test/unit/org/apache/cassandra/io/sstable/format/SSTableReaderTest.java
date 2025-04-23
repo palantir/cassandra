b25c4a5416579b3c74a9362980428c3f36112179
ines potier <ipotier@palantir.com>
2025-04-23 16:06:52 -0400

    Set loadNewSstable true in unit tests

--- test/unit/org/apache/cassandra/io/sstable/format/SSTableReaderTest.java
+++ test/unit/org/apache/cassandra/io/sstable/format/SSTableReaderTest.java
@@ -219,7 +219,7 @@ public class SSTableReaderTest
     private void clearAndLoad(ColumnFamilyStore cfs)
     {
         cfs.clearUnsafe();
-        cfs.loadNewSSTables();
+        cfs.loadNewSSTables(true);
     }
 
     @Test
