e37299faf33174852f43712024d53130fc847ec5
Daniel Guo <danielguo@palantir.com>
2025-04-23 16:27:22 -0400

    rename nit

--- src/java/org/apache/cassandra/db/filter/SliceQueryFilter.java
+++ src/java/org/apache/cassandra/db/filter/SliceQueryFilter.java
@@ -329,7 +329,7 @@ public class SliceQueryFilter implements IDiskAtomFilter
         // otherwise set it to a cell name if we hit one of the defensive guards
 
         CellName firstCell = null;
-        CellName lastScannedCellInContainer = null;
+        CellName lastSeenCellInContainer = null;
         while (!columnCounter.hasSeenAtLeast(count) && reducedCells.hasNext())
         {
             Cell cell = reducedCells.next();
@@ -344,7 +344,7 @@ public class SliceQueryFilter implements IDiskAtomFilter
             {
                 assert cell.name() != firstCell :
                         "Hit the range scan threshold on the first cell. Either the configured threshold is too low or there are unexpected duplicate cells.";
-                assert lastScannedCellInContainer == null || cell.name() != lastScannedCellInContainer :
+                assert lastSeenCellInContainer == null || cell.name() != lastSeenCellInContainer :
                         "Hit the range scan threshold on a cell that is included in the results set. This should never happen.";
 
                 container.setPageToken(cell);
@@ -381,7 +381,7 @@ public class SliceQueryFilter implements IDiskAtomFilter
             }
 
             container.appendColumn(cell);
-            lastScannedCellInContainer = cell.name();
+            lastSeenCellInContainer = cell.name();
 
             if (LOG_HIGH_MEMORY_COLLECTION)
             {
