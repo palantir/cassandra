c6859034c9c64655d0fde58c4a44f6f695b16bef
Ryan Norris <rynorris@gmail.com>
2025-05-12 13:45:55 +0100

    Use tracing.istracing

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -2423,7 +2423,11 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
         assert !(range.keyRange() instanceof Range) || !((Range<?>)range.keyRange()).isWrapAround() || range.keyRange().right.isMinimum() : range.keyRange();
 
         final ViewFragment view = select(viewFilter(range.keyRange()));
-        Tracing.trace("Executing seq scan across {} sstables", view.sstables.size());
+
+        if (Tracing.isTracing())
+        {
+            Tracing.trace("Executing seq scan across {} sstables for {}", view.sstables.size(), range.keyRange().getString(metadata.getKeyValidator()));
+        }
 
         final CloseableIterator<Row> iterator = RowIteratorFactory.getIterator(view.memtables, view.sstables, range, this, now);
 
