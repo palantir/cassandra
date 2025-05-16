9eec4e01063b45eafbdd9b2da8cb660d04656fe2
Andy Bradshaw <abradshaw@palantir.com>
2025-05-16 09:40:53 -0400

    wip: inter-node tracing

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -44,6 +44,12 @@ import com.palantir.cassandra.cvim.CrossVpcIpMappingAck;
 import com.palantir.cassandra.cvim.CrossVpcIpMappingSyn;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.UnsafeArg;
+import com.palantir.tracing.CloseableSpan;
+import com.palantir.tracing.CloseableTracer;
+import com.palantir.tracing.Detached;
+import com.palantir.tracing.DetachedSpan;
+import com.palantir.tracing.Tracers;
+import com.palantir.tracing.api.SpanType;
 import org.apache.cassandra.concurrent.ExecutorLocals;
 import org.apache.cassandra.concurrent.ScheduledExecutors;
 import org.apache.cassandra.concurrent.Stage;
@@ -71,6 +77,7 @@ import org.apache.cassandra.security.SSLFactory;
 import org.apache.cassandra.service.*;
 import org.apache.cassandra.service.paxos.Commit;
 import org.apache.cassandra.service.paxos.PrepareResponse;
+import org.apache.cassandra.tracing.PalantirTracing;
 import org.apache.cassandra.tracing.TraceState;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.*;
@@ -678,8 +685,14 @@ public final class MessagingService implements MessagingServiceMBean
      */
     public int sendRR(MessageOut message, InetAddress to, IAsyncCallback cb, long timeout, boolean failureCallback)
     {
+        // n.b. this will be closed on the traced callback
+        CloseableTracer ignored = CloseableTracer.startSpan("MessagingService#sendRR", SpanType.CLIENT_OUTGOING);
+        cb = new TracedCallback<>(cb, DetachedSpan.detach());
         int id = addCallback(cb, message, to, timeout, failureCallback);
-        sendOneWay(failureCallback ? message.withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE) : message, id, to);
+        sendOneWay(failureCallback ?
+                   message.withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE).withParameters(PalantirTracing.serializeForMessage()) :
+                   message.withParameters(PalantirTracing.serializeForMessage()),
+                   id, to);
         return id;
     }
 
@@ -700,8 +713,15 @@ public final class MessagingService implements MessagingServiceMBean
                       AbstractWriteResponseHandler<?> handler,
                       boolean allowHints)
     {
-        int id = addCallback(handler, message, to, message.getTimeout(), handler.consistencyLevel, allowHints);
-        sendOneWay(message.withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE), id, to);
+        // n.b. this will be closed on the traced callback
+        CloseableTracer ignored = CloseableTracer.startSpan("MessagingService#sendRR", SpanType.CLIENT_OUTGOING);
+        IAsyncCallbackWithFailure<?> cb = new TracedCallback<>(handler, DetachedSpan.detach());
+        int id = addCallback(cb, message, to, message.getTimeout(), handler.consistencyLevel, allowHints);
+        sendOneWay(message
+                   .withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE)
+                   // TODO(abradshaw): figure out if we can just overwrite the span id here... or maybe
+                   .withParameters(PalantirTracing.serializeForMessage()),
+                   id, to);
         return id;
     }
 
@@ -800,6 +820,7 @@ public final class MessagingService implements MessagingServiceMBean
     public void receive(MessageIn message, int id, long timestamp, boolean isCrossNodeTimestamp)
     {
         TraceState state = Tracing.instance.initializeFromMessage(message);
+        PalantirTracing.initializeTracerFromMessage("MessageService#recieve", message);
         if (state != null)
             state.trace("{} message received from {}", SafeArg.of("verb", message.verb), SafeArg.of("endpoint", message.from));
 
@@ -808,7 +829,7 @@ public final class MessagingService implements MessagingServiceMBean
             if (!ms.allowIncomingMessage(message, id))
                 return;
 
-        Runnable runnable = new MessageDeliveryTask(message, id, timestamp, isCrossNodeTimestamp);
+        Runnable runnable = Tracers.wrap(new MessageDeliveryTask(message, id, timestamp, isCrossNodeTimestamp));
         LocalAwareExecutorService stage = StageManager.getStage(message.getMessageType());
         assert stage != null : "No stage for message type " + message.verb;
 
