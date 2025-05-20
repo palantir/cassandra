32d8f47573dbdfc66445b41941bc9dde6ddaa96d
ines potier <ipotier@palantir.com>
2025-05-20 13:14:08 -0400

    Clarify log message

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -5207,7 +5207,8 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     public void loadNewSSTables(String ksName, String cfName, boolean assumeCfIsEmpty)
     {
         if(!isNewCluster())
-            throw new RuntimeException("Failure: is_new_cluster config is false. Using loadnewsstable on active production clusters is unsafe.");
+            throw new RuntimeException("Failure: is_new_cluster config is false. "
+                                        + "Using loadnewsstable on active production cluster could lead to sstable/data conflicts.");
         if (!isInitialized())
             throw new RuntimeException("StorageService not yet initialized, can't load new sstables");
         ColumnFamilyStore.loadNewSSTables(ksName, cfName, assumeCfIsEmpty);
