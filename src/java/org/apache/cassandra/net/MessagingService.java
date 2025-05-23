90bf87ddfe5e7e974a5248b32a46a8d3d44821e5
Andy Bradshaw <abradshaw@palantir.com>
2025-05-23 10:55:12 -0400

    Update inter-node tracing

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -44,7 +44,9 @@ import com.palantir.cassandra.cvim.CrossVpcIpMappingAck;
 import com.palantir.cassandra.cvim.CrossVpcIpMappingSyn;
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.UnsafeArg;
+import com.palantir.tracing.CloseableTracer;
 import com.palantir.tracing.DetachedSpan;
+import com.palantir.tracing.Tracer;
 import com.palantir.tracing.Tracers;
 import com.palantir.tracing.api.SpanType;
 import org.apache.cassandra.concurrent.ExecutorLocals;
@@ -684,10 +686,10 @@ public final class MessagingService implements MessagingServiceMBean
     {
         // n.b. this will be closed on the traced callback
         DetachedSpan span = DetachedSpan.start("MessagingService#sendRR", SpanType.CLIENT_OUTGOING);
-        cb = new TracedCallback<>(cb, span);
+        cb = new TracedCallback<>(message, cb, span);
         int id = addCallback(cb, message, to, timeout, failureCallback);
-        sendOneWay(failureCallback ?
-                   message.withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE).withParameters(PalantirTracing.serializeForMessage()) :
+        sendOneWay(failureCallback ? message.withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE)
+                                            .withParameters(PalantirTracing.serializeForMessage()) :
                    message.withParameters(PalantirTracing.serializeForMessage()),
                    id, to);
         return id;
@@ -712,7 +714,7 @@ public final class MessagingService implements MessagingServiceMBean
     {
         // n.b. this will be closed on the traced callback
         DetachedSpan span = DetachedSpan.start("MessagingService#sendRR", SpanType.CLIENT_OUTGOING);
-        IAsyncCallbackWithFailure<?> cb = new TracedCallback<>(handler, span);
+        IAsyncCallbackWithFailure<?> cb = new TracedCallback<>(message, handler, span);
         int id = addCallback(cb, message, to, message.getTimeout(), handler.consistencyLevel, allowHints);
         sendOneWay(message
                    .withParameter(FAILURE_CALLBACK_PARAM, ONE_BYTE)
@@ -741,23 +743,25 @@ public final class MessagingService implements MessagingServiceMBean
      */
     public void sendOneWay(MessageOut message, int id, InetAddress to)
     {
-        if (logger.isTraceEnabled())
-            logger.trace("{} sending {} to {}@{}", SafeArg.of("endpoint", FBUtilities.getBroadcastAddress()), SafeArg.of("verb", message.verb),
-                         SafeArg.of("id", id), SafeArg.of("to", to));
+        try (CloseableTracer ignored = CloseableTracer.startSpan("MessagingService#sendOneWay")) {
+            if (logger.isTraceEnabled())
+                logger.trace("{} sending {} to {}@{}", SafeArg.of("endpoint", FBUtilities.getBroadcastAddress()), SafeArg.of("verb", message.verb),
+                             SafeArg.of("id", id), SafeArg.of("to", to));
 
-        if (to.equals(FBUtilities.getBroadcastAddress()))
-            logger.trace("Message-to-self {} going over MessagingService", message);
+            if (to.equals(FBUtilities.getBroadcastAddress()))
+                logger.trace("Message-to-self {} going over MessagingService", message);
 
-        // message sinks are a testing hook
-        for (IMessageSink ms : messageSinks)
-            if (!ms.allowOutgoingMessage(message, id, to))
-                return;
+            // message sinks are a testing hook
+            for (IMessageSink ms : messageSinks)
+                if (!ms.allowOutgoingMessage(message, id, to))
+                    return;
 
-        // get pooled connection (really, connection queue)
-        OutboundTcpConnection connection = getConnection(to, message);
+            // get pooled connection (really, connection queue)
+            OutboundTcpConnection connection = getConnection(to, message);
 
-        // write it
-        connection.enqueue(message, id);
+            // write it
+            connection.enqueue(message, id);
+        }
     }
 
     public <T> AsyncOneResponse<T> sendRR(MessageOut message, InetAddress to)
@@ -826,11 +830,12 @@ public final class MessagingService implements MessagingServiceMBean
             if (!ms.allowIncomingMessage(message, id))
                 return;
 
-        Runnable runnable = Tracers.wrap(new MessageDeliveryTask(message, id, timestamp, isCrossNodeTimestamp));
+        Runnable runnable = Tracers.wrap("MessageDeliveryTask#run", new MessageDeliveryTask(message, id, timestamp, isCrossNodeTimestamp));
         LocalAwareExecutorService stage = StageManager.getStage(message.getMessageType());
         assert stage != null : "No stage for message type " + message.verb;
 
         stage.execute(runnable, ExecutorLocals.create(state));
+        Tracer.fastCompleteSpan();
     }
 
     public void setCallbackForTests(int messageId, CallbackInfo callback)
