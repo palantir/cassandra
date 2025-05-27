8b69c202e0d68f71e9c8edda9f2cfd99806dede7
Leon Cheung <lcheung@palantir.com>
2025-05-27 19:47:57 -0400

    added some comments

--- src/java/org/apache/cassandra/service/opstate/KeyspaceTableOpStatePersister.java
+++ src/java/org/apache/cassandra/service/opstate/KeyspaceTableOpStatePersister.java
@@ -137,6 +137,8 @@ public class KeyspaceTableOpStatePersister
         {
             tmpFile.createNewFile();
             Files.write(tmpFile.toPath(), content.getBytes(), StandardOpenOption.TRUNCATE_EXISTING);
+            // exception thrown here due to the .tmp file not existing, but the .tmp file did successfully
+            // move to the real json file.
             Files.move(tmpFile.toPath(), file.toPath(),
                        StandardCopyOption.ATOMIC_MOVE, StandardCopyOption.REPLACE_EXISTING);
         }
