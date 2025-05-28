68bd27d79b291a8bc6b15605c42c2346ed868406
ines potier <ipotier@palantir.com>
2025-05-28 10:40:03 -0400

    Add unit test

--- src/java/org/apache/cassandra/db/compaction/CompactionController.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionController.java
@@ -19,6 +19,7 @@ package org.apache.cassandra.db.compaction;
 
 import java.util.*;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
@@ -51,7 +52,7 @@ public class CompactionController implements AutoCloseable
 
     public final ColumnFamilyStore cfs;
     // note that overlapIterator and overlappingSSTables will be null if NEVER_PURGE_TOMBSTONES is set - this is a
-    // good thing so that noone starts using them and thinks that if overlappingSSTables is empty, there
+    // good thing so that no one starts using them and thinks that if overlappingSSTables is empty, there
     // is no overlap.
     private Refs<SSTableReader> overlappingSSTables;
     private OverlapIterator<RowPosition, SSTableReader> overlapIterator;
@@ -140,7 +141,7 @@ public class CompactionController implements AutoCloseable
             return Collections.<SSTableReader>emptySet();
 
         if (doNotPurgeTombstones(cfStore)) {
-            logger.debug("not looking for droppable sstables - doNotPurgeTombstones returned true for keyspace {}", cfStore.keyspace.getName());
+            logger.debug("Not looking for droppable sstables - doNotPurgeTombstones returned true for keyspace {}", cfStore.keyspace.getName());
             return Collections.<SSTableReader>emptySet();
         }
 
@@ -268,7 +269,8 @@ public class CompactionController implements AutoCloseable
     /**
      * @param cfs
      * @return true if this node may be streaming data for this keyspace, or if cassandra.never_purge_tombstones is set
-     * If we are streaming, tombstones should not be purged so that we don't pre-maturely purge those that exceed gc_grace_seconds.
+     * If the node is streaming, tombstones should not be purged. If we pre-maturely purge a tombstone that was written
+     * at time T+1, and the node started streaming at time T, we may end up with resurrected keys.
      * <p>
      * Note that this node may receive data without streaming, e.g. during a repair.
      */
