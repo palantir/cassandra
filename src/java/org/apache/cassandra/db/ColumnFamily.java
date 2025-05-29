cc5318528e54200daa1e6d7a0c1c70033f34e9b6
Daniel Guo <dguo2000@yahoo.com>
2025-05-29 11:56:16 -0700

    Use `assertWithError` instead of `assert` for LRRS code paths (#706)
    Co-authored-by: daniel guo <danielguo@palantir.com>

--- src/java/org/apache/cassandra/db/ColumnFamily.java
+++ src/java/org/apache/cassandra/db/ColumnFamily.java
@@ -136,22 +136,22 @@ public abstract class ColumnFamily implements Iterable<Cell>, IRowCacheEntry
 
     public void setPageToken(PageToken pageToken)
     {
-        assert pageToken != null;
+        Throwables.assertWithError(pageToken != null);
 
         this.pageToken = pageToken;
     }
 
     public void setPageToken(Cell cell)
     {
-        assert cell != null;
-        assert pageToken == null;
+        Throwables.assertWithError(cell != null);
+        Throwables.assertWithError(pageToken == null);
 
         pageToken = PageToken.createPageToken(cell);
     }
 
     public void setPageTokenEndOfRow()
     {
-        assert pageToken == null;
+        Throwables.assertWithError(pageToken == null);
 
         pageToken = PageToken.createPageTokenReachedEnd();
     }
