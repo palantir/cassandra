9ae83814a37c8f98cb18d0586632eafcbefa0bf0
daniel guo <danielguo@palantir.com>
2025-06-06 12:09:33 -0400

    also for announce

--- src/java/org/apache/cassandra/service/MigrationManager.java
+++ src/java/org/apache/cassandra/service/MigrationManager.java
@@ -231,10 +231,10 @@ public class MigrationManager
     public static boolean shouldPullSchemaFrom(InetAddress endpoint)
     {
         /*
-         * Don't request schema from nodes with an incompatible or unknown major version (may have incompatible schema)
+         * Don't request schema from nodes with an incompatible or unknown major version
          * Don't request schema from fat clients
          */
-        return compatibleVersionsForSchemaPull(endpoint) && !Gossiper.instance.isGossipOnlyMember(endpoint);
+        return compatibleMessagingVersionsForSchema(endpoint) && !Gossiper.instance.isGossipOnlyMember(endpoint);
     }
 
     public static boolean shouldPullSchemaFrom(InetAddress endpoint, UUID theirVersion)
@@ -248,13 +248,13 @@ public class MigrationManager
         Set<InetAddress> currentlyScheduledRequests = scheduledSchemaPulls.getOrDefault(theirVersion, Collections.emptySet());
         boolean noScheduledRequests = currentlyScheduledRequests.size() < MAX_SCHEDULED_SCHEMA_PULL_REQUESTS
                                       && !currentlyScheduledRequests.contains(endpoint);
-        return compatibleVersionsForSchemaPull(endpoint)
+        return compatibleMessagingVersionsForSchema(endpoint)
                 && !Gossiper.instance.isGossipOnlyMember(endpoint)
                 && !Schema.emptyVersion.equals(theirVersion)
                 && noScheduledRequests;
     }
 
-    private static boolean compatibleVersionsForSchemaPull(InetAddress endpoint)
+    private static boolean compatibleMessagingVersionsForSchema(InetAddress endpoint)
     {
         if (!MessagingService.instance().knowsVersion(endpoint))
         {
@@ -263,10 +263,10 @@ public class MigrationManager
 
         int otherVersion = MessagingService.instance().getRawVersion(endpoint);
 
-        return compatibleVersionsForSchemaPull(otherVersion);
+        return compatibleMessagingVersionsForSchema(otherVersion);
     }
 
-    private static boolean compatibleVersionsForSchemaPull(int otherVersion)
+    private static boolean compatibleMessagingVersionsForSchema(int otherVersion)
     {
         return otherVersion == MessagingService.current_version || (SCHEMA_COMPATIBLE_VERSIONS_RRS.contains(otherVersion) && SCHEMA_COMPATIBLE_VERSIONS_RRS.contains(MessagingService.current_version));
     }
@@ -630,12 +630,10 @@ public class MigrationManager
 
                 try (CloseableTracer ignored1 = CloseableTracer.startSpan("MigrationManager#announce compute condition"))
                 {
-                    condition = !endpoint.equals(FBUtilities.getBroadcastAddress()) &&
-                            MessagingService.instance().knowsVersion(endpoint) &&
-                            MessagingService.instance().getRawVersion(endpoint) == MessagingService.current_version;
+                    condition = !endpoint.equals(FBUtilities.getBroadcastAddress()) && compatibleMessagingVersionsForSchema(endpoint);
                 }
 
-                // only push schema to nodes with known and equal versions
+                // only push schema to nodes with known and compatible versions
                 if (condition) {
                     logger.debug("Anouncing schema to endpoint {}", SafeArg.of("endpoint", endpoint));
                     logger.trace("Announcing schema {}", UnsafeArg.of("schema", schema));
